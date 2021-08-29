087a7a26570852cd38c232a9f3ce21bd1cdf9c73
gill984 <mjgill984@gmail.com>
2021-08-29 14:28:46 -0400

    Time: 2 ms (99.46%), Space: 44.3 MB (80.35%) - LeetHub

---
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        // Can mod all numbers by 60 wlog
+        int [] count = new int[60];
+        for (int i = 0; i < time.length; i++)
+            count[time[i] % 60] += 1;
+        
+        int result = 0;
+        if(count[0] > 0)
+            result += (count[0] * (count[0] - 1)) / 2;
+        if(count[30] > 0)
+            result += (count[30] * (count[30] - 1)) / 2;
+        
+        for (int i = 1; i <= 29; i++)
+            result += count[i] * count[60 - i];
+        
+        return result;
+    }
+}
