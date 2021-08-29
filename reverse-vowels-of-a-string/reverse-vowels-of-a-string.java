e0e6d30db28f3fdeb269c487644645109e7ad572
gill984 <mjgill984@gmail.com>
2021-08-29 12:28:49 -0400

    Time: 2 ms (99.17%), Space: 39 MB (78.73%) - LeetHub

--- reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -3,21 +3,23 @@ class Solution {
         char [] sChars = s.toCharArray();
         int lo = 0;
         int hi = s.length() - 1;
-        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
         
         while (lo < hi)
         {
-            while (lo < sChars.length && lo < hi && !vowels.contains(sChars[lo]))
+            while (lo < sChars.length && lo < hi && !isVowel(sChars[lo]))
                 lo++;
-            while (hi >= 0 && lo < hi && !vowels.contains(sChars[hi]))
+            while (hi >= 0 && lo < hi && !isVowel(sChars[hi]))
                 hi--;
             
             if (lo < sChars.length && lo < hi && hi >= 0)
             {
                 // XOR swap
-                sChars[lo] ^= sChars[hi];
-                sChars[hi] ^= sChars[lo];
-                sChars[lo] ^= sChars[hi];
+                // sChars[lo] ^= sChars[hi];
+                // sChars[hi] ^= sChars[lo];
+                // sChars[lo] ^= sChars[hi];
+                char temp = sChars[lo];
+                sChars[lo] = sChars[hi];
+                sChars[hi] = temp;
             }
             
             lo++;
@@ -25,5 +27,11 @@ class Solution {
         }
         
         return new String(sChars);
+    }
+    
+    public boolean isVowel(char c)
+    {
+        return (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' ||
+                c == 'o' || c == 'O' || c == 'u' || c == 'U');
     }
 }
