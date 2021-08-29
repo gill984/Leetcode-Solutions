a3c64c84d017659b554e8f714021395a70b0d39f
gill984 <mjgill984@gmail.com>
2021-08-29 14:44:16 -0400

    Time: 2 ms (99.46%), Space: 51.7 MB (11.86%) - LeetHub

--- pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
+++ pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -11,7 +11,7 @@ class Solution {
         if(count[30] > 0)
             result += (count[30] * (count[30] - 1)) / 2;
         
-        for (int i = 1; i <= 29; i++)
+        for (int i = 1; i < 30; i++)
             result += count[i] * count[60 - i];
         
         return result;
