e4fdcf9d266378119b3647799110d2c8241f5d47
gill984 <mjgill984@gmail.com>
2021-09-08 15:19:44 -0400

    Time: 12 ms (21.45%), Space: 41 MB (16.16%) - LeetHub

--- best-meeting-point/best-meeting-point.java
+++ best-meeting-point/best-meeting-point.java
@@ -3,29 +3,20 @@ class Solution {
         List<Integer> rows = new ArrayList<Integer>();
         List<Integer> cols = new ArrayList<Integer>();
         for (int row = 0; row < grid.length; row++)
-        {
             for (int col = 0; col < grid[0].length; col++)
-            {
                 if (grid[row][col] == 1)
-                {
                     rows.add(row);
-                    cols.add(col);
-                }
-            }
-        }
         
-        Collections.sort(rows);
-        Collections.sort(cols);
+        for (int col = 0; col < grid[0].length; col++)
+            for (int row = 0; row < grid.length; row++)
+                if (grid[row][col] == 1)
+                    cols.add(col);
+        
         int rowMedian = 0;
         int colMedian = 0;
-        for (int lo = 0, hi = rows.size() - 1; lo <= hi; lo++, hi--)
-        {
-            rowMedian = (rows.get(lo) + rows.get(hi)) / 2;
-            colMedian = (cols.get(lo) + cols.get(hi)) / 2;
-        }
         
-        // System.out.println(rowMedian);
-        // System.out.println(colMedian);
+        rowMedian = rows.get(rows.size() / 2);
+        colMedian = cols.get(cols.size() / 2);
         
         int res = 0;
         for (int i = 0; i < rows.size(); i++)
