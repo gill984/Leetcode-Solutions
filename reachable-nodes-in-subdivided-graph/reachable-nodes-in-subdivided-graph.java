da64d353a9825a900d0c103f948ed3e80b142595
gill984 <mjgill984@gmail.com>
2021-09-12 11:32:26 -0400

    Time: 190 ms (14.97%), Space: 82.8 MB (43.11%) - LeetHub

--- reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.java
+++ reachable-nodes-in-subdivided-graph/reachable-nodes-in-subdivided-graph.java
@@ -1,17 +1,11 @@
 class Solution {
-    Map<Integer, Integer> reached;
-    List<Map<Integer, Integer>> graph;
-    int [] visited;
     int n;
-    int res;
-    int maxMoves;
-    
     public int reachableNodes(int[][] edges, int maxMoves, int n) {
         this.n = n;
-        this.maxMoves = maxMoves;
-        res = 0;
-        reached = new HashMap<>();
-        graph = new ArrayList<>();
+        Map<Integer, Integer> reached = new HashMap<>();
+        List<Map<Integer, Integer>> graph = new ArrayList<>();
+        boolean [] visited = new boolean[n];
+        int res = 0;
         
         for (int i = 0; i < n; i++)
             graph.add(new HashMap<>());
@@ -24,88 +18,46 @@ class Solution {
             reached.put(hash(e[1], e[0]), 0);
         }
         
-        visited = new int[n];
-        Arrays.fill(visited, -1);
-        
-        dijkstra();
-        
-        return res;
-    }
-    
-    public void dijkstra()
-    {
-        PriorityQueue<Path> pq = new PriorityQueue<Path>(n, (a, b) -> a.moves - b.moves);
-        pq.add(new Path(0, 0));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(n, (a, b) -> a[1] - b[1]);
+        pq.add(new int[] {0, 0});
         
         while (!pq.isEmpty())
         {
-            // System.out.println(pq);
-            Path p = pq.poll();
-            int node = p.node;
-            int moves = p.moves;
+            int[] p = pq.poll();
+            int node = p[0];
+            int moves = p[1];
             
-            // Either it's the first time we've been to this node,
-            // we've been here with more moves made and should continue,
-            // or we've been here with fewer moves made and need to stop
-            if (visited[node] == -1) {
-                res += 1;
-                visited[node] = moves;
-            }
-            else if (visited[node] > moves)
-            {
-                visited[node] = moves;
-            } 
-            else
-            {
+            if (visited[node])
                 continue;
-            }
 
-            // For each neighbor of n, nbr; determine if we can reach that nbr and how many unreached
-            // subdivided nodes we can reach between n and nbr
+            res += 1;
+            visited[node] = true;
             int movesRemaining = maxMoves - moves;
             Map<Integer, Integer> neighbors = graph.get(node);
             for (int nbr : neighbors.keySet())
             {
-                // First determine how many subdivided nodes we can reach and add them
-                int distance = neighbors.get(nbr);
+                int d = neighbors.get(nbr);
                 int reachedFromN = reached.get(hash(node, nbr));
                 int reachedFromNbr = reached.get(hash(nbr, node));
-                int nodesRemaining = Math.max(distance - reachedFromN - reachedFromNbr, 0);
+                int nodesRemaining = d - reachedFromN - reachedFromNbr;
 
                 if (nodesRemaining > 0)
                 {
-                    int newNodes = Math.min(movesRemaining - reachedFromN, nodesRemaining);
-                    reached.put(hash(node, nbr), reachedFromN + newNodes);
-                    res += newNodes;
+                    int newReached = Math.min(movesRemaining - reachedFromN, nodesRemaining);
+                    reached.put(hash(node, nbr), reachedFromN + newReached);
+                    res += newReached;
                 }
 
-                if (movesRemaining >= distance + 1 && visited[nbr] == -1 || visited[nbr] > moves + distance + 1)
-                {
-                    pq.offer(new Path(nbr, moves + distance + 1));
-                }
+                if (movesRemaining >= d + 1 && !visited[nbr])
+                    pq.offer(new int[] {nbr, moves + d + 1});
             }
         }
+        
+        return res;
     }
     
     public int hash(int from, int to)
     {
-        return (from) + (to * n);
-    }
-    
-    class Path
-    {
-        int node;
-        int moves;
-        
-        public Path(int id, int m)
-        {
-            node = id;
-            moves = m;
-        }
-        
-        public String toString()
-        {
-            return "Node: " + node + ", moves: " + moves; 
-        }
+        return (from) + (to * n * 1007);
     }
 }
