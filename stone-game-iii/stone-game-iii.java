6cff8265ecb74f3a30c6a265f31925b12a859761
gill984 <mjgill984@gmail.com>
2021-09-14 17:59:45 -0400

    Time: 63 ms (28.94%), Space: 124.3 MB (8.12%) - LeetHub

--- stone-game-iii/stone-game-iii.java
+++ stone-game-iii/stone-game-iii.java
@@ -1,20 +1,23 @@
 class Solution {
+    final int SENTINEL = Integer.MIN_VALUE;
     int n;
     int [] presum;
     int[] stoneValue;
-    Map<Integer, Integer> memo = new HashMap<>();
+    int[] memo;
     
     public String stoneGameIII(int[] stoneValue) {
         n = stoneValue.length;
         this.stoneValue = stoneValue;
         presum = new int [n];
+        memo = new int[n];
+        Arrays.fill(memo, SENTINEL);
         for (int i = n - 2; i >= 0; i--)
             presum[i] = presum[i] + presum[i + 1];
         
         int alice = dfs(0);
-        if (alice > 0)
+        if (2 * alice > presum[0])
             return "Alice";
-        else if (alice == 0)
+        else if (2 * alice == presum[0])
             return "Tie";
         else
             return "Bob";
@@ -22,21 +25,25 @@ class Solution {
     
     public int dfs(int stone)
     {
-        if (memo.containsKey(stone))
-            return memo.get(stone);
-        else if (stone >= n)            // No stones left
+        if (stone >= n)            // No stones left
             return 0;
+        if (memo[stone] != SENTINEL)
+            return memo[stone];
         
-        int sum = stoneValue[stone];
-        int value = sum - dfs(stone + 1);
         
-        for (int take = stone + 1; take < Math.min(stone + 3, n); take++)
+        int res = Integer.MIN_VALUE;
+        for (int take = 0; take < 3; take++)
         {
-            sum += stoneValue[take];
-            value = Math.max(value, sum - dfs(take + 1));
+            int one = stoneValue[stone];
+            int two = (take > 0 && stone + 1 < n) ? stoneValue[stone + 1] : 0;
+            int three = (take > 1 && stone + 2 < n) ? stoneValue[stone + 2] : 0;
+            
+            int pickup = one + two + three;
+            int remainingStones = (stone + take + 1 < n) ? presum[stone + take + 1] : 0;
+            res = Math.max(res, pickup + (remainingStones - dfs(stone + take + 1)));
         }
         
-        memo.put(stone, value);
-        return value;
+        memo[stone] = res;
+        return res;
     }
 }
