45b2dff83cfc1986c3f38dd4b841b71c0ea7d35b
gill984 <mjgill984@gmail.com>
2021-09-15 16:32:57 -0400

    Time: 25 ms (40.96%), Space: 129.6 MB (15.06%) - LeetHub

--- maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.java
+++ maximum-sum-obtained-of-any-permutation/maximum-sum-obtained-of-any-permutation.java
@@ -1,46 +1,25 @@
-class Solution {
-    final int START = 0;
-    final int END = 1;
-    final long MOD = 1000000007;
-    
+class Solution {    
     public int maxSumRangeQuery(int[] nums, int[][] requests) {
         int n = nums.length;
-        Arrays.sort(requests, (a, b) -> a[0] - b[0]);
-        Arrays.sort(nums);
-        
         int [] counts = new int[n];
-        PriorityQueue<Integer> ends = new PriorityQueue<>();
-        for (int r = 0; r < requests.length; r++)
+        
+        for (int[] r : requests)
         {
-            int index = requests[r][START];
-            ends.offer(requests[r][END]);
-            
-            while (index < n && !ends.isEmpty())
-            {
-                while (!ends.isEmpty() && index > ends.peek())
-                {
-                    ends.poll();
-                }
-                
-                while (r + 1 < requests.length && index >= requests[r + 1][START])
-                {
-                    ends.offer(requests[r + 1][END]);
-                    r += 1;
-                }
-                
-                counts[index] = ends.size();
-                index++;
-            }
+            counts[r[0]] += 1;
+            if (r[1] + 1 < n)
+                counts[r[1] + 1] -= 1; 
         }
         
+        for (int i = 1; i < n; i++)
+            counts[i] = counts[i] + counts[i - 1];
+        
+        Arrays.sort(nums);
         Arrays.sort(counts);
-        // System.out.println(Arrays.toString(counts));
-        // System.out.println(Arrays.toString(nums));
         
         long res = 0;
-        for (int i = n - 1; i >= 0; i--)
-            res = (res + ( (long) nums[i] * (long) counts[i])) % MOD;
+        for (int i = n - 1; i >= 0 && counts[i] > 0 && nums[i] > 0; i--)
+            res += (long) nums[i] * counts[i];
         
-        return (int) res;
+        return (int) (res % 1000000007);
     }
 }
