38f11c507b4ee4fb71e6514ab21ec30b1b1504d8
gill984 <mjgill984@gmail.com>
2021-09-16 09:37:39 -0400

    Time: 35 ms (14.96%), Space: 39.5 MB (60.68%) - LeetHub

--- minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.java
+++ minimize-max-distance-to-gas-station/minimize-max-distance-to-gas-station.java
@@ -4,26 +4,19 @@ class Solution
     public double minmaxGasDist(int[] stations, int k)
     {
         int n = stations.length;
-        double hi = 0.0;
+        double hi = stations[n - 1];
         double lo = (stations[n - 1] - stations[0]) / (1.0 * n + k);
-        for (int i = 0; i + 1 < n; i++)
-            hi = Math.max(hi, stations[i + 1] - stations[i]);
-        
         double res = hi;
-        while (lo < hi)
+        while (lo + 1E-6 < hi)
         {
             double mid = (lo + hi) / 2;
             double fits = attemptDistance(mid, stations, k);
             if (fits == FAIL)
-            {
                 lo = mid;
-            }
             else
             {
                 res = Math.min(res, fits);
                 hi = mid;
-                if ((hi - lo) < 0.000001)
-                    return res;
             }
         }
         
