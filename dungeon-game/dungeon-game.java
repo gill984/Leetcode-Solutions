dcd94eab3fe3ad8b9a30b7915375fb4a1cddc190
gill984 <mjgill984@gmail.com>
2021-10-02 10:01:50 -0400

    Time: 6 ms (7.66%), Space: 41.2 MB (18.29%) - LeetHub

--- dungeon-game/dungeon-game.java
+++ dungeon-game/dungeon-game.java
@@ -1,41 +1,36 @@
 class Solution {
-    final int ROW = 0;
-    final int COL = 1;
+    int [][] dirs = new int [][] {{-1, 0}, {0, -1}};
+    int ROW = 0;
+    int COL = 1;
     public int calculateMinimumHP(int[][] dungeon) {
         int m = dungeon.length;
         int n = dungeon[0].length;
         int [][] dp = new int[m][n];
-        dp[m - 1][n - 1] = Math.max(1, -dungeon[m - 1][n - 1] + 1);
+        dp[m - 1][n - 1] = Math.max(1, 1 - dungeon[m - 1][n - 1]);
+        
         for (int row = m - 1; row >= 0; row--)
         {
             for (int col = n - 1; col >= 0; col--)
             {
-                int nbrRow = row + -1;
-                int nbrCol = col + 0;
-
-                if (nbrRow >= 0 && nbrCol >= 0)
+                // Look up and left
+                for (int[] dir : dirs)
                 {
-                    int cell = dungeon[nbrRow][nbrCol];
-                    int path = Math.max(dp[row][col] - cell, 1);
-                    int saved = dp[nbrRow][nbrCol];
-                    if (saved == 0 || path < saved)
-                        dp[nbrRow][nbrCol] = path;
-                }
-                
-                // Look at up and left directions
-                nbrRow = row;
-                nbrCol = col - 1;
-
-                if (nbrRow >= 0 && nbrCol >= 0)
-                {
-                    int cell = dungeon[nbrRow][nbrCol];
-                    int path = Math.max(dp[row][col] - cell, 1);
-                    int saved = dp[nbrRow][nbrCol];
-                    if (saved == 0 || path < saved)
-                        dp[nbrRow][nbrCol] = path;
+                    int nbrRow = row + dir[ROW];
+                    int nbrCol = col + dir[COL];
+                    
+                    if (nbrRow >= 0 && nbrCol >= 0)
+                    {
+                        if (dp[nbrRow][nbrCol] == 0)
+                            dp[nbrRow][nbrCol] = Math.max(dp[row][col] - dungeon[nbrRow][nbrCol], 1);
+                        else
+                            dp[nbrRow][nbrCol] = Math.min(Math.max(dp[row][col] - dungeon[nbrRow][nbrCol], 1), dp[nbrRow][nbrCol]);
+                    }
                 }
             }
         }
+        
+        // for (int [] a : dp)
+        //     System.out.println(Arrays.toString(a));
         
         return dp[0][0];
     }
