48695ea09a9604cc658dfecd9ea98592617a8bfa
gill984 <mjgill984@gmail.com>
2021-10-06 21:57:22 -0400

    Time: 107 ms (63.32%), Space: 37 MB (76.96%) - LeetHub

---
+++ word-search/word-search.java
@@ -0,0 +1,55 @@
+class Solution {
+    int [][] dirs = new int [][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        char [] w = word.toCharArray();
+        
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (board[i][j] == w[0])
+                {
+                    if (dfs(board, w, i, j, 0))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean dfs (char[][] board, char[] w, int row, int col, int wIdx)
+    {
+        int m = board.length;
+        int n = board[0].length;
+        if (wIdx == w.length - 1)
+            return true;
+        
+        char temp = board[row][col];
+        board[row][col] = '0';
+        
+        for (int[] dir : dirs)
+        {
+            int nbrRow = row + dir[0];
+            int nbrCol = col + dir[1];
+            
+            if (nbrRow >= 0 && nbrRow < m && nbrCol >= 0 && nbrCol < n)
+            {
+                if (board[nbrRow][nbrCol] == w[wIdx + 1])
+                {
+                    boolean complete = dfs(board, w, nbrRow, nbrCol, wIdx + 1);
+                    if (complete)
+                        return true;
+                }
+            }
+        }
+        
+        board[row][col] = temp;
+        return false;
+    }
+}
