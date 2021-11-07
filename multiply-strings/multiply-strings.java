f35459c55a3097e962fb2fa93fea68ed25536c4e
gill984 <mjgill984@gmail.com>
2021-11-07 00:56:26 -0400

    Time: 11 ms (33.58%), Space: 39.7 MB (29.76%) - LeetHub

--- multiply-strings/multiply-strings.java
+++ multiply-strings/multiply-strings.java
@@ -2,8 +2,7 @@ class Solution {
     public String multiply(String num1, String num2) {
         char [] a = num1.toCharArray();
         char [] b = num2.toCharArray();
-        char [] c = new char [a.length + b.length];
-        Arrays.fill(c, '0');
+        int [] c = new int [a.length + b.length];
         
         for (int i = 0; i < a.length; i++)
         {
@@ -17,17 +16,15 @@ class Solution {
                 
                 int product = aDigit * bDigit;
                 
-                System.out.println(product);
-                
                 c[c.length - 1 - place] += product % 10;
-                if (c[c.length - 1 - place] > '9')
+                if (c[c.length - 1 - place] > 9)
                 {
                     c[c.length - 2 - place] += 1;
                     c[c.length - 1 - place] -= 10;
                 }
                 
                 c[c.length - 2 - place] += product / 10;
-                if (c[c.length - 2 - place] > '9')
+                if (c[c.length - 2 - place] > 9)
                 {
                     c[c.length - 3 - place] += 1;
                     c[c.length - 2 - place] -= 10;
@@ -35,14 +32,24 @@ class Solution {
             }
         }
         
-        int start = 0;
-        for (; start < c.length; start++)
-            if (c[start] != '0')
-                break;
+        boolean nonZeroFound = false;
+        StringBuilder res = new StringBuilder();
+        for (int start = 0; start < c.length; start++)
+        {
+            if (c[start] != 0)
+            {
+                nonZeroFound = true;
+                res.append(c[start]);
+            }
+            else if (nonZeroFound)
+            {
+                res.append(c[start]);
+            }
+        }
         
-        if (start == c.length)
+        if (res.length() == 0)
             return "0";
-        
-        return new String(c, start, c.length - start);
+        else
+            return res.toString();
     }
 }
