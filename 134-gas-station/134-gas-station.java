970a7d30682a719d8c99eb0de6a40cd1e417b7b0
gill984 <mjgill984@gmail.com>
2022-01-20 19:56:05 -0500

    Time: 9 ms (8.78%), Space: 63.7 MB (39.94%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,14 +1,5 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // Let's say we're trying out values from a certain starting position
-        // if we find that this position doesn't work, can skip to the next position which comes after a negative margin position.
-        // Where margin[i] = gas[i] - cost[i].
-        // Can skip those other values because this value doesn't work and provides a positive margin.
-        // Can all values up to the current endpoint be skipped?
-        
-        // If an index j exists s.t. j > i and j < k, where k is the current end point which causes negative margin.
-        // Then j also cannot be the starting point, because the cumulative margin at j is >= 0 in order to reach k,
-        // so all values i-k are eliminated as possible starting points
         int n = gas.length;
         for (int start = 0; start < n; start++)
         {
