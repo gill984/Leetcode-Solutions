7fd8f6d4caf705ba05b1959b88f7943d6fbf2545
gill984 <mjgill984@gmail.com>
2022-02-07 00:13:39 -0500

    Time: 1 ms (99.79%), Space: 42.7 MB (6.09%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,17 +1,12 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int [] count = new int [26];
+        char res = 0;
         for (char c : t.toCharArray())
-            count[c - 'a'] += 1;
+            res ^= c;
         
         for (char c : s.toCharArray())
-            count[c - 'a'] -= 1;
+            res ^= c;
         
-        int i = 0;
-        for (; i < count.length; i++) 
-            if (count[i] == 1)
-                break;
-        
-        return (char) ('a' + i);
+        return res;
     }
 }
