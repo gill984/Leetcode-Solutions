5fc770d74c07d770424135d111c8aef321dc9eac
gill984 <mjgill984@gmail.com>
2022-02-21 20:30:52 -0500

    Time: 1 ms (17.08%), Space: 45.4 MB (13.50%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.java
+++ 1064-fixed-point/1064-fixed-point.java
@@ -1,8 +1,16 @@
 class Solution {
     public int fixedPoint(int[] arr) {
         for (int i = 0; i < arr.length; i++)
+        {
             if (arr[i] == i)
+            {
                 return i;
+            }
+            else if (arr[i] > i)
+            {
+                i = arr[i] + 1;
+            }
+        }
         return -1;
     }
 }
