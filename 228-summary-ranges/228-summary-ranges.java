64ded6fda16117e1c6429b3f7dcf9947d57e2d6a
gill984 <mjgill984@gmail.com>
2022-02-27 23:06:03 -0500

    Time: 10 ms (39.32%), Space: 42.4 MB (21.59%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -3,19 +3,14 @@ class Solution {
         List<String> res = new ArrayList<String>();
         for (int i = 0; i < nums.length;)
         {
-            int j = i + 1;
-            while (j < nums.length && nums[j] == nums[j - 1] + 1)
+            int j = i;
+            while (j + 1 < nums.length && nums[j] + 1 == nums[j + 1])
                 j++;
-            j -= 1;
             
             if (i == j)
-            {
                 res.add("" + nums[i]);
-            }
             else
-            {
                 res.add(nums[i] + "->" + nums[j]);
-            }
             
             i = j + 1;
         }
