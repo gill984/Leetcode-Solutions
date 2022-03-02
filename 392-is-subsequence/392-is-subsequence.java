001a3082ab57fc3216bfe1e630adb8866c01439d
gill984 <mjgill984@gmail.com>
2022-03-02 10:17:54 -0500

    Time: 1 ms (88.36%), Space: 41.3 MB (45.55%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -3,11 +3,8 @@ class Solution {
         int i = 0, j = 0;
         while(i < s.length() && j < t.length())
         {
-            if (s.charAt(i) == t.charAt(j))
-            {
+            if (s.charAt(i) == t.charAt(j++))
                 i++;
-            }
-            j++;
         }
         return i == s.length();
     }
