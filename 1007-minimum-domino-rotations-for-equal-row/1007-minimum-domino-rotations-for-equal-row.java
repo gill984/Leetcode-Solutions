c3c2542f5eeda418d1950de8cd415bab0d5a59cf
gill984 <mjgill984@gmail.com>
2022-03-19 21:55:43 -0400

    Time: 5 ms (79.08%), Space: 88.6 MB (70.49%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.java
@@ -1,66 +1,90 @@
 class Solution {
-    public int minDominoRotations(int[] A, int[] B) {
-        int aSwaps = 0;
-        int bSwaps = 0;
+    public int minDominoRotations(int[] tops, int[] bottoms) {
+        int n = tops.length;
         
-        int attempt = A[0];
-        int aBothMatch = 0;
-        for (int i = 0; i < A.length; i++)
+        // Top row same as top[0]
+        int res = Integer.MAX_VALUE;
+        int target = tops[0];
+        int count = 0;
+        for (int i = 0; i < n; i++)
         {
-            if (A[i] == attempt && B[i] == attempt)
-            {
-                aBothMatch += 1;
-                continue;
-            }
-            else if (A[i] == attempt)
+            if (tops[i] == target)
             {
                 continue;
             }
-            else if (B[i] == attempt)
+            else if (bottoms[i] == target)
             {
-                aSwaps += 1;
+                count++;
             }
             else
             {
-                aSwaps = -1;
-                break;
-            }
-        }
-        aSwaps = Math.min(aSwaps, A.length - aSwaps - aBothMatch);
-        
-        int bBothMatch = 0;
-        attempt = B[0];
-        for (int i = 0; i < B.length; i++)
-        {
-            if (A[i] == attempt && B[i] == attempt)
-            {
-                bBothMatch += 1;
-                continue;
-            }
-            if (B[i] == attempt)
-            {
-                continue;
-            }
-            else if (A[i] == attempt)
-            {
-                bSwaps += 1;
-            }
-            else
-            {
-                bSwaps = -1;
+                count = -1;
                 break;
             }
         }
         
-        bSwaps = Math.min(bSwaps, B.length - bSwaps - bBothMatch);
+        res = (count != -1 ? Math.min(res, count) : res);
         
-        if (aSwaps == -1 && bSwaps == -1)
-            return -1;
-        else if (aSwaps == -1)
-            return bSwaps;
-        else if (bSwaps == -1)
-            return aSwaps;
-        else
-            return Math.min(aSwaps, bSwaps);
+        count = 0;
+        target = bottoms[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (bottoms[i] == target)
+            {
+                continue;
+            }
+            else if (tops[i] == target)
+            {
+                count++;
+            }
+            else
+            {
+                count = -1;
+                break;
+            }
+        }
+        res = (count != -1 ? Math.min(res, count) : res);
+        
+        count = 0;
+        target = tops[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (bottoms[i] == target)
+            {
+                continue;
+            }
+            else if (tops[i] == target)
+            {
+                count++;
+            }
+            else
+            {
+                count = -1;
+                break;
+            }
+        }
+        res = (count != -1 ? Math.min(res, count) : res);
+        
+        count = 0;
+        target = bottoms[0];
+        for (int i = 0; i < n; i++)
+        {
+            if (tops[i] == target)
+            {
+                continue;
+            }
+            else if (bottoms[i] == target)
+            {
+                count++;
+            }
+            else
+            {
+                count = -1;
+                break;
+            }
+        }
+        res = (count != -1 ? Math.min(res, count) : res);
+        
+        return (res == Integer.MAX_VALUE ? -1 : res);
     }
 }
