fb29d2b76f2137d483406b28438226860d0ee641
gill984 <mjgill984@gmail.com>
2022-03-23 22:10:37 -0400

    Time: 14 ms (97.12%), Space: 50.6 MB (87.30%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -1,30 +1,20 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        TreeSet<Integer> set = new TreeSet<>();
-        Map<Integer, Integer> counts = new HashMap<>();
-        
-        for (int p : people)
-        {
-            set.add(p);
-            counts.put(p, counts.getOrDefault(p, 0) + 1);
-        }
-            
+        Arrays.sort(people);
+        int lo = 0;
+        int hi = people.length - 1;
         int res = 0;
-        while (!set.isEmpty())
+        
+        while (hi >= lo)
         {
             res++;
-            int curr = set.last();
-            counts.put(curr, counts.get(curr) - 1);
-            if (counts.get(curr) == 0)
-                set.remove(curr);
+            int heavy = people[hi];
+            int light = people[lo];
+            hi--;
             
-            int max = limit - curr;
-            int choice = set.floor(max) != null ? set.floor(max) : -1;
-            if (choice != -1)
+            if (heavy + light <= limit)
             {
-                counts.put(choice, counts.get(choice) - 1);
-                if (counts.get(choice) == 0)
-                    set.remove(choice);
+                lo++;
             }
         }
         
