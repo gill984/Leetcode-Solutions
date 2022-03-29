386d1948b4c39abd98e72dcb880d42cb9c6bbb68
gill984 <mjgill984@gmail.com>
2022-03-28 21:21:58 -0400

    Time: 4 ms (94.53%), Space: 75.5 MB (65.25%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,33 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Put each number in it's index location
-        // if the correct number is already there, this is the duplicate
-        // store the extra number in the original location
-        int idx = 0;
-        int n = nums.length - 1;
         
-        while (idx < n + 1)
-        {
-            int num1 = nums[idx];
-            if (num1 - 1 != idx)
-            {
-                int num2 = nums[num1 - 1];
-                if (num2 == num1)
-                {
-                    return num1;
-                }
-                else
-                {
-                    nums[idx] = num2;
-                    nums[num1 - 1] = num1;
-                }
-            }
-            else
-            {
-                idx++;
-            }
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+        
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        tortoise = nums[0];
+        
+        while (tortoise != hare) {
+            tortoise = nums[tortoise];
+            hare = nums[hare];
         }
-        
-        return 0;
+
+        return hare;
     }
 }
