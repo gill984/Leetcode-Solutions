4dfeda2ca7f189eaae69656e21271091f91f564b
gill984 <mjgill984@gmail.com>
2022-04-08 00:01:28 -0400

    Time: 5 ms (96.47%), Space: 92.4 MB (79.21%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
@@ -1,11 +1,32 @@
 class Solution {
     public int minProductSum(int[] nums1, int[] nums2) {
+        int [] bucket1 = new int [100];
+        int [] bucket2 = new int [100];
         int n = nums1.length;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int res = 0;
+        
         for (int i = 0; i < n; i++)
-            res += nums1[i] * nums2[n - 1 - i];
+        {
+            bucket1[nums1[i] - 1]++;
+            bucket2[nums2[i] - 1]++;
+        }
+        
+        int res = 0;
+        int lo = 0;
+        int hi = 99;
+        
+        for (int i = 0; i < n; i++)
+        {
+            // move lo and hi to next available bucket
+            while (bucket1[lo] == 0)
+                lo++;
+            while (bucket2[hi] == 0)
+                hi--;
+            
+            res += (hi + 1) * (lo + 1);
+            bucket1[lo]--;
+            bucket2[hi]--;
+        }
+        
         return res;
     }
 }
