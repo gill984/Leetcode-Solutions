3162671f98947d4b394a9ee0f962d2186f26123f
gill984 <mjgill984@gmail.com>
2022-04-07 23:45:13 -0400

    Time: 32 ms (14.97%), Space: 52.5 MB (50.10%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -33,9 +33,3 @@ class KthLargest {
         return minHeap.peek();
     }
 }
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
