330b6949ea600a6dc462bb08148bcba476e7af42
gill984 <mjgill984@gmail.com>
2022-04-10 10:01:49 -0400

    Time: 10 ms (5.89%), Space: 42.1 MB (68.25%) - LeetHub

--- 682-baseball-game/682-baseball-game.java
+++ 682-baseball-game/682-baseball-game.java
@@ -1,19 +1,20 @@
 class Solution {
     public int calPoints(String[] ops) {
-        List<Integer> nums = new ArrayList<>();
+        List<Integer> nums = new LinkedList<>();
         for (String op : ops)
         {
+            int l = nums.size();
             if (op.equals("+"))
             {
-                nums.add(nums.get(nums.size() - 1) + nums.get(nums.size() - 2));
+                nums.add(nums.get(l - 1) + nums.get(l - 2));
             }
             else if (op.equals("D"))
             {
-                nums.add(nums.get(nums.size() - 1) * 2);
+                nums.add(nums.get(l - 1) * 2);
             }
             else if (op.equals("C"))
             {
-                nums.remove(nums.get(nums.size() - 1));
+                nums.remove(nums.get(l - 1));
             }
             else
             {
