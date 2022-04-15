245645ee3aed106375afc7801c564e7c7a0c66cc
gill984 <mjgill984@gmail.com>
2022-04-14 21:01:34 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (81.50%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -7,18 +7,12 @@ class Solution {
             return null;
         
         if (root.val < low)
-        {
             return trimBST(root.right, low, high);
-        }
         else if (root.val > high)
-        {
             return trimBST(root.left, low, high);
-        }
-        else
-        {
-            root.left = trimBST(root.left, low, high);
-            root.right = trimBST(root.right, low, high);
-            return root;
-        }
+        
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
     }
 }
