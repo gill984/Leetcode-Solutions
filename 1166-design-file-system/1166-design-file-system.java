1a954b1d88e4015bb5dd196f68807f0d3cc84cad
gill984 <mjgill984@gmail.com>
2022-04-21 21:36:58 -0400

    Time: 560 ms (5.05%), Space: 145.9 MB (5.12%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.java
@@ -0,0 +1,71 @@
+class FileSystem {
+    Dir root;
+    
+    
+    class Dir
+    {
+        String name;
+        int value;
+        Map<String, Dir> subdirs;
+        
+        public Dir (String name, int value)
+        {
+            this.name = name;
+            this.value = value;
+            subdirs = new HashMap<>();
+        }
+        
+        public boolean add (Dir d)
+        {
+            if (subdirs.containsKey(d.name))
+                return false;
+            System.out.println("Adding " + d.name);
+            subdirs.put(d.name, d);
+            return true;
+        }
+    }
+
+    public FileSystem() {
+        root = new Dir("", 0);
+    }
+    
+    public boolean createPath(String path, int value) {
+        String [] tokens = path.split("/");
+        // System.out.println(Arrays.toString(tokens));
+        Dir curr = root;
+        for (int i = 1; i < tokens.length - 1; i++)
+        {
+            String t = tokens[i];
+            if (curr.subdirs.containsKey(t))
+                curr = curr.subdirs.get(t);
+            else
+                return false;
+        }
+        //System.out.println(tokens[tokens.length - 1]);
+        return curr.add(new Dir(tokens[tokens.length - 1], value));
+    }
+    
+    public int get(String path) {
+        String [] tokens = path.split("/");
+        //System.out.println(Arrays.toString(tokens));
+        Dir curr = root;
+        for (int i = 1; i < tokens.length; i++)
+        {
+            String t = tokens[i];
+            System.out.println(curr.subdirs);
+            if (curr.subdirs.containsKey(t))
+                curr = curr.subdirs.get(t);
+            else
+                return -1;
+        }
+        
+        return curr.value;
+    }
+}
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem obj = new FileSystem();
+ * boolean param_1 = obj.createPath(path,value);
+ * int param_2 = obj.get(path);
+ */
