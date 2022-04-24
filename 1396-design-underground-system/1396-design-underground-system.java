798a4db9380b386452558d8bcee7e87f583c8502
gill984 <mjgill984@gmail.com>
2022-04-24 00:03:44 -0400

    Time: 147 ms (70.03%), Space: 104.1 MB (29.01%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -0,0 +1,74 @@
+class UndergroundSystem {
+    Map<Integer, Timestamp> checkins;
+    Map<String, Fraction> times;
+    
+    
+    public UndergroundSystem() {
+        checkins = new HashMap<>();
+        times = new HashMap<>();
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+        checkins.put(id, new Timestamp(t, stationName));
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+        Timestamp ts = checkins.get(id);
+        String key = ts.station + "," + stationName;
+        int dt = t - ts.time;
+        if (times.containsKey(key))
+        {
+            times.get(key).add(dt);
+        }
+        else
+        {
+            times.put(key, new Fraction(dt));
+        }
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        return times.get(startStation + "," + endStation).getAverage();
+    }
+    
+    class Timestamp
+    {
+        int time;
+        String station;
+        
+        public Timestamp (int t, String s) {
+            time = t;
+            station = s;
+        }
+    }
+    
+    class Fraction
+    {
+        int numerator;
+        int denominator;
+        
+        public Fraction(int n)
+        {
+            numerator = n;
+            denominator = 1;
+        }
+        
+        public void add (int n)
+        {
+            numerator += n;
+            denominator += 1;
+        }
+        
+        public double getAverage()
+        {
+            return (double) ((double) numerator / (double) denominator);
+        }
+    }
+}
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
