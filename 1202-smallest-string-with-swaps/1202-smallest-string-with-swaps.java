2f266a5719b96cf0bf2e505940203c107366e1ae
gill984 <mjgill984@gmail.com>
2022-04-26 21:46:08 -0400

    Time: 59 ms (68.35%), Space: 89.7 MB (97.84%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -2,41 +2,32 @@ class Solution {
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
         // Union the cycles and sort them
         int n = s.length();
-        DS disjointSet = new DS(n);
-        for (List<Integer> pair : pairs) {
-            disjointSet.union(pair.get(0), pair.get(1));
-        }
+        DS ds = new DS(n);
+        for (List<Integer> pair : pairs)
+            ds.union(pair.get(0), pair.get(1));
         
         Map<Integer, PriorityQueue<Character>> map = new HashMap<>();
-        for (int i = 0; i < n; i++)
-        {
-            int group = disjointSet.root(i);
+        for (int i = 0; i < n; i++) {
+            int group = ds.root(i);
             map.putIfAbsent(group, new PriorityQueue<Character>());
             map.get(group).offer(s.charAt(i));
         }
         
-        // System.out.println(map);
-        
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < n; i++)
-        {
-            res.append(map.get(disjointSet.root(i)).poll());
-        }
+            res.append(map.get(ds.root(i)).poll());
         
         return res.toString();
     }
 }
 
-class DS
-{
+class DS {
     int [] p;
     
-    public DS (int size)
-    {
+    public DS (int size) {
         p = new int [size];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size; i++)
             p[i] = i;
-        }
     }
     
     public void union (int a, int b) {
