c4a4cb3997906ee4adee9f1da28a57c760cb4565
gill984 <mjgill984@gmail.com>
2022-05-01 23:42:48 -0400

    Time: 2 ms (38.15%), Space: 48.6 MB (28.56%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -4,13 +4,15 @@ class Solution {
         int n = nums.length;
         for (int i = 0; i < n;) {
             if (nums[i] % 2 == 0) {
-                int temp = nums[i];
-                nums[i] = nums[evenIdx];
-                nums[evenIdx] = temp;
                 if (evenIdx == i) {
                     i++;
+                    evenIdx++;
+                } else {
+                    int temp = nums[i];
+                    nums[i] = nums[evenIdx];
+                    nums[evenIdx] = temp;
+                    evenIdx++;
                 }
-                evenIdx++;
             } else {
                 i++;
             }
