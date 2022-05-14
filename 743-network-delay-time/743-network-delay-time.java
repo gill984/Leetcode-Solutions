c62c2994652834b500cd786f5bc04e2f8f730e8c
gill984 <mjgill984@gmail.com>
2022-05-13 23:42:30 -0400

    Time: 26 ms (51.77%), Space: 67.6 MB (16.33%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Node[] nodes = new Node[n + 1];
+        for (int i = 0; i < n + 1; i++)
+            nodes[i] = new Node();
+        
+        for (int[] time : times) {
+            nodes[time[0]].neighbors.add(new int [] {time[1], time[2]});
+        }
+        
+        // Just run dijkstra pussy
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
+        pq.offer(new int [] {k, 0});
+        Set<Integer> visited = new HashSet<>();
+        
+        while (!pq.isEmpty()) {
+            // System.out.println(visited);
+            // for (int [] a : pq)
+            //     System.out.println(Arrays.toString(a));
+            // System.out.println();
+            
+            int [] path = pq.poll();
+            if (visited.contains(path[0])) {
+                continue;
+            }
+            
+            visited.add(path[0]);
+            if (visited.size() == n) {
+                return path[1];
+            }
+            
+            if (nodes[path[0]].neighbors == null)
+                continue;
+            
+            for (int [] nbr : nodes[path[0]].neighbors) {
+                pq.offer(new int [] {nbr[0], path[1] + nbr[1]});
+            }
+        }
+        
+        return -1;
+    }
+    
+    class Node {
+        List<int[]> neighbors;
+        public Node () {
+            neighbors = new ArrayList<>();
+        }
+    }
+}
