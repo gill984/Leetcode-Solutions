ed5b1fc2b0454b77437b010fd5a8194e09528432
gill984 <mjgill984@gmail.com>
2022-05-13 23:59:01 -0400

    Time: 12 ms (91.26%), Space: 45.4 MB (93.22%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -4,9 +4,8 @@ class Solution {
         for (int i = 0; i < n + 1; i++)
             nodes[i] = new Node();
         
-        for (int[] time : times) {
+        for (int[] time : times)
             nodes[time[0]].neighbors.add(new int [] {time[1], time[2]});
-        }
         
         // Just run dijkstra pussy
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> a[1] - b[1]);
@@ -15,20 +14,16 @@ class Solution {
         
         while (!pq.isEmpty()) {
             int [] path = pq.poll();
-            if (visited.contains(path[0])) {
+            if (visited.contains(path[0]))
                 continue;
-            }
             
             visited.add(path[0]);
-            if (visited.size() == n) {
+            if (visited.size() == n)
                 return path[1];
-            }
             
-            for (int [] nbr : nodes[path[0]].neighbors) {
-                if (!visited.contains(nbr[0])) {
+            for (int [] nbr : nodes[path[0]].neighbors)
+                if (!visited.contains(nbr[0]))
                     pq.offer(new int [] {nbr[0], path[1] + nbr[1]});
-                }
-            }
         }
         
         return -1;
