69cfa0dfad424c742b5bb54b97fe5571c3e851df
gill984 <mjgill984@gmail.com>
2022-05-23 14:03:50 -0400

    Time: 28 ms (83.84%), Space: 42.7 MB (71.92%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -2,23 +2,22 @@ class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
         int [][] dp = new int [m + 1][n + 1];
         int [][] counts = new int [strs.length][2];
-        for (int i = 0; i < strs.length; i++) {
+        for (int i = 0; i < strs.length; i++) 
             counts[i] = getCounts(strs[i]);
-        }
+        
         for (int i = 0; i <= m; i++)
             Arrays.fill(dp[i], -1);
-        dp[0][0] = 0;
         
+        dp[0][0] = 0;
         int max = 0;
         for (int [] count : counts) {
             for (int i = m; i >= 0; i--) {
                 for (int j = n; j >= 0; j--) {
-                    if (dp[i][j] == -1) {
+                    if (dp[i][j] == -1)
                         continue;
-                    }
+
                     int z = i + count[0];
                     int o = j + count[1];
-                    
                     if (z <= m && o <= n) {
                         dp[z][o] = Math.max(dp[z][o], dp[i][j] + 1);
                         max = Math.max(max, dp[z][o]);
@@ -33,11 +32,10 @@ class Solution {
     public int[] getCounts(String s) {
         int [] res = new int [2];
         for (char c : s.toCharArray()) {
-            if (c == '0') {
+            if (c == '0')
                 res[0]++;
-            } else {
+            else
                 res[1]++;
-            }
         }
         return res;
     }
