27b11717b610ba0525491f36892c305b9670d5cf
gill984 <mjgill984@gmail.com>
2022-05-28 00:05:15 -0400

    Time: 1 ms (65.97%), Space: 42.8 MB (96.54%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int zero = -1;
+        
+        // Swap the numbers into the correct index
+        // swap to index val - 1, swap 0 into zdx
+        for (int i = 0; i < n;) {
+            if (nums[i] == 0) {
+                zero = 0;
+                nums[i] = -1;
+                i++;
+            } else if (nums[i] == i + 1 || nums[i] == -1) {
+                i++;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        
+        if (zero == -1) {
+            return 0;
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == -1)
+                return i + 1;
+        }
+        
+        return -1;
+    }
+}
