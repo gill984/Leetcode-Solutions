6d99e9c554074e1f159fddb9746171e715cb1172
gill984 <mjgill984@gmail.com>
2022-05-28 21:25:08 -0400

    Time: 13 ms (61.32%), Space: 53.9 MB (66.90%) - LeetHub

---
+++ 1136-parallel-courses/1136-parallel-courses.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int minimumSemesters(int n, int[][] relations) {
+        // Topological sort on the dependency graph
+        Node [] nodes = new Node[n + 1];
+        
+        for (int i = 0; i < n + 1; i++) {
+            nodes[i] = new Node();
+        }
+        
+        for (int i = 0; i < relations.length; i++) {
+            int preReq = relations[i][0];
+            int postReq = relations[i][1];
+            nodes[preReq].postReqs.add(postReq);
+            nodes[postReq].inDegree++;
+        }
+        
+        // Perform topological sort
+        int visited = 0;
+        int semesters = 0;
+        ArrayDeque<Integer> next = new ArrayDeque<>();
+        
+        for (int i = 1; i < n + 1; i++) {
+            if (nodes[i].inDegree == 0) {
+                next.offer(i);
+            }
+        }        
+        
+        while (!next.isEmpty()) {
+            semesters++;
+            ArrayDeque<Integer> curr = next;
+            next = new ArrayDeque<>();
+            
+            while (!curr.isEmpty()) {
+                int node = curr.poll();
+                for (int nbr : nodes[node].postReqs) {
+                    nodes[nbr].inDegree--;
+                    if (nodes[nbr].inDegree == 0) {
+                        next.offer(nbr);
+                    }
+                }
+                visited++;
+            }
+        }
+        
+        return visited == n ? semesters : -1;
+    }
+    
+    class Node {
+        int inDegree;
+        List<Integer> postReqs;
+        
+        public Node () {
+            inDegree = 0;
+            postReqs = new ArrayList<>();
+        }
+    }
+}
