c2d5062050b7719ac989548d87f8858a3a8bb360
gill984 <mjgill984@gmail.com>
2022-05-30 16:46:06 -0400

    Time: 1 ms (97.73%), Space: 42 MB (79.07%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
@@ -3,16 +3,14 @@ class Solution {
         int n = number.length();
         int place = 0;
         for (int i = 0; i < n; i++) {
-            if (number.charAt(i) != digit) {
+            if (number.charAt(i) != digit)
                 continue;
-            }
             
             place = i;
-            if (i + 1 == n) {
+            if (i + 1 == n)
                 return number.substring(0, i);
-            } else if (number.charAt(i + 1) > digit) {
-                return number.substring(0, i) + number.substring(i + 1, n);
-            }
+            else if (number.charAt(i + 1) > digit)
+                break;
         }
         
         return (number.substring(0, place) + number.substring(place + 1, n));
