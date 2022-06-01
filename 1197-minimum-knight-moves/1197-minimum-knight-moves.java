16addf014140068a4c487c22b4c4f1cfc381e801
gill984 <mjgill984@gmail.com>
2022-05-31 21:47:09 -0400

    Time: 250 ms (63.58%), Space: 116.2 MB (53.45%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.java
@@ -0,0 +1,42 @@
+class Solution {
+    int MIN_BOUND = -300;
+    int MAX_BOUND = 300;
+    int [][] moves = new int [][] {{2, 1}, {1, 2}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+    
+    public int minKnightMoves(int x, int y) {
+        boolean [][] visited = new boolean[601][601];
+        Deque<int[]> next = new ArrayDeque<>();
+        int [] start = new int [] {0, 0};
+        visited[300][300] = true;
+        next.offer(start);
+        int steps = 0;
+        
+        while (!next.isEmpty()) {
+            Deque<int[]> curr = next;
+            next = new ArrayDeque<>();
+            while (!curr.isEmpty()) {
+                int [] cand = curr.poll();
+                if (cand[0] == x && cand[1] == y) {
+                    return steps;
+                }
+
+                for (int [] move : moves) {
+                    int nextX = cand[0] + move[0];
+                    int nextY = cand[1] + move[1];
+
+                    if (nextX < MIN_BOUND || nextX > MAX_BOUND ||
+                        nextY < MIN_BOUND || nextY > MAX_BOUND ||
+                        visited[nextX + 300][nextY + 300]) {
+                        continue;
+                    }
+                    
+                    visited[nextX + 300][nextY + 300] = true;
+                    next.offer(new int [] {nextX, nextY});
+                }
+            }
+            steps++;
+        }
+        
+        return steps;
+    }
+}
