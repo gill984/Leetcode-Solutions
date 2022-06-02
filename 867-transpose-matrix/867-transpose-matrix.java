0c28f04684e0fa0289be4e6192eada25f7af4b1a
gill984 <mjgill984@gmail.com>
2022-06-01 20:11:17 -0400

    Time: 1 ms (52.16%), Space: 47.3 MB (71.99%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.java
+++ 867-transpose-matrix/867-transpose-matrix.java
@@ -5,11 +5,9 @@ class Solution {
         int n = matrix[0].length;
         int [][] res = new int [n][m];
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
                 res[j][i] = matrix[i][j];
-            }
-        }
         
         return res;
     }
