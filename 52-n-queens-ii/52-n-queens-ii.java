d27a7e6b77a525697af52dbdc44160c292e46d48
gill984 <mjgill984@gmail.com>
2022-06-05 12:18:55 -0400

    Time: 2 ms (84.08%), Space: 41 MB (51.20%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.java
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -1,15 +1,16 @@
 class Solution {
     int n;
-    int usedCols = 0, usedDiagDown = 0, usedDiagUp = 0, res = 0;
+    int res = 0;
+    short usedCols = 0, usedDiagDown = 0, usedDiagUp = 0;
     
     public int totalNQueens(int n) {
         this.n = n;
-        dfs(0);
+        dfs((short) 0);
         return res;
     }
     
-    final public void dfs (int row) {
-        for (int col = 0; col < n; col++) {
+    public void dfs (short row) {
+        for (short col = 0; col < n; col++) {
             if (isUsed(row, col))
                 continue;
 
@@ -18,17 +19,19 @@ class Solution {
             if (row == n - 1)
                 res++;
             else
-                dfs (row + 1);
+                dfs ((short) (row + 1));
 
             flipUseBits(row, col);
         }
     }
     
-    final public boolean isUsed (int row, int col) {
-        return (usedCols & 1 << col) != 0 || (usedDiagDown & 1 << (n - 1 - row + col)) != 0 || (usedDiagUp & (1 << (row + col))) != 0;
+    final private boolean isUsed (short row, short col) {
+        return (usedCols & 1 << col) != 0 ||
+            (usedDiagDown & 1 << (n - 1 - row + col)) != 0 ||
+            (usedDiagUp & (1 << (row + col))) != 0;
     }
     
-    final public void flipUseBits (int row, int col) {
+    final private void flipUseBits (short row, short col) {
         usedCols ^= (1 << col);
         usedDiagDown ^= (1 << (n - 1 - row + col));
         usedDiagUp ^= (1 << (row + col));
