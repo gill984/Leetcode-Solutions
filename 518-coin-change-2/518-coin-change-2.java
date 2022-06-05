73448977d95f51ddaacafe58b809ec19fbacad0a
gill984 <mjgill984@gmail.com>
2022-06-05 12:45:05 -0400

    Time: 4 ms (78.32%), Space: 41.2 MB (83.45%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -3,9 +3,8 @@ class Solution {
         int [] dp = new int [amount + 1];
         dp[0] = 1;
         for (int coin : coins)
-            for (int i = 0; i <= amount - coin; i++)
-                if (dp[i] != 0)
-                    dp[i + coin] += dp[i];
+            for (int i = coin; i <= amount; i++)
+                dp[i] += dp[i - coin];
 
         return dp[amount];
     }
