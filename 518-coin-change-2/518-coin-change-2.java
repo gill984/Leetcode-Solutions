c777b377597c4027cb0aa8ee9888abe874420478
gill984 <mjgill984@gmail.com>
2022-06-05 12:41:52 -0400

    Time: 3 ms (89.91%), Space: 42.3 MB (57.67%) - LeetHub

---
+++ 518-coin-change-2/518-coin-change-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int [] dp = new int [amount + 1];
+        dp[0] = 1; // empty set AKA base case
+        for (int coin : coins) {
+            for (int i = 0; i <= amount - coin; i++) {
+                if (dp[i] != 0) {
+                    dp[i + coin] += dp[i];
+                }
+            }
+        }
+        return dp[amount];
+    }
+}
