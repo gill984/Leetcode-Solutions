e9a0443e491895ccbd0a0e42743ab9c3cfe769b5
gill984 <mjgill984@gmail.com>
2022-06-08 21:28:31 -0400

    Time: 5 ms (16.09%), Space: 50.4 MB (23.58%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,30 +1,19 @@
 class Solution {
     public int[] twoSum(int[] numbers, int k) {
-        Map<Integer, int[]> indices = new HashMap<>();
+        int [] res = new int [2];
+        Map<Integer, Integer> indices = new HashMap<>();
+        
         for (int i = 0; i < numbers.length; i++) {
             int num = numbers[i];
-            indices.putIfAbsent(num, new int [] {0, 0});
-            if (indices.get(num)[0] == 0) {
-                indices.get(num)[0] = i + 1;
-            } else {
-                indices.get(num)[1] = i + 1;
-            }
-        }
-        
-        int [] res = new int [2];
-        for (int i : indices.keySet()) {
-            if (indices.containsKey(k - i) && (k - i != i || indices.get(i)[1] != 0)) {
-                res[0] = indices.get(i)[0];
-                res[1] = (k - i == i ? indices.get(i)[1] : indices.get(k - i)[0]);
+            
+            if (indices.containsKey(k - num)) {
+                res[0] = indices.get(k - num);
+                res[1] = i + 1;
                 break;
             }
+            indices.put(num, i + 1);
         }
         
-        if (res[0] > res[1]) {
-            int swap = res[0];
-            res[0] = res[1];
-            res[1] = swap;
-        }
         return res;
     }
 }
