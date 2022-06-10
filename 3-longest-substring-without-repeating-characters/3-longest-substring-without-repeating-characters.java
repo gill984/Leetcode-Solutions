d75ef71ec9d2f097d972444fe5c6000260b27ef6
gill984 <mjgill984@gmail.com>
2022-06-09 20:54:00 -0400

    Time: 3 ms (97.49%), Space: 44.1 MB (64.14%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -5,9 +5,8 @@ class Solution {
         
         for (int hi = 0, lo = 0; hi < s.length(); hi++) {
             char c = s.charAt(hi);
-            if (index[c] != 0) {
+            if (index[c] != 0)
                 lo = Math.max(lo, index[c]);
-            }
             index[c] = hi + 1;
             res = Math.max(hi - lo + 1, res);
         }
