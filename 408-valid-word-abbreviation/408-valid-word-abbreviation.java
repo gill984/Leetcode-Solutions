9776de20c0bd410841902265e68a2a5690100e91
gill984 <mjgill984@gmail.com>
2022-06-11 16:42:29 -0400

    Time: 1 ms (98.52%), Space: 42.6 MB (22.30%) - LeetHub

---
+++ 408-valid-word-abbreviation/408-valid-word-abbreviation.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0, j = 0;
+        for (; i < word.length() && j < abbr.length(); i++, j++) {
+            if (Character.isDigit(abbr.charAt(j))) {
+                if (abbr.charAt(j) == '0')
+                    return false;
+                
+                int num = abbr.charAt(j) - '0';
+                while (j + 1 < abbr.length() && Character.isDigit(abbr.charAt(j + 1))) {
+                    j++;
+                    num = (num * 10) + (abbr.charAt(j) - '0');
+                }
+                i += num - 1;
+            } else if (word.charAt(i) != abbr.charAt(j)) {
+                return false;
+            }
+        }
+        
+        return i == word.length() && j == abbr.length();
+    }
+}
