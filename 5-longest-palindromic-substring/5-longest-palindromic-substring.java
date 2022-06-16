80d900f5487ee5d334f2be9e53c006c33cc949cf
gill984 <mjgill984@gmail.com>
2022-06-15 20:38:12 -0400

    Time: 55 ms (54.52%), Space: 53.7 MB (38.17%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -6,16 +6,12 @@ class Solution {
         
         for (int i = 0; i < 2; i++) {
             for (int mid = 0; mid < n; mid++) {
-                int length = 0;
                 for (int lo = mid, hi = mid + i; lo >= 0 && hi < n; lo--, hi++) {
                     if (s.charAt(lo) != s.charAt(hi))
                         break;
                     
-                    length += (lo == hi) ? 1 : 2;
-                    
-                    if (length > res.length()) {
+                    if (hi - lo + 1 > res.length())
                         res = s.substring(lo, hi + 1);
-                    }
                 }
             }
         }
