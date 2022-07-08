d824214840ee6a9083d3c00fc0b17bbf685a1610
gill984 <mjgill984@gmail.com>
2022-07-08 00:25:57 -0400

    Time: 49 ms (49.38%), Space: 51.5 MB (37.50%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,45 +1,43 @@
 class Solution {
+    final int UNVISITED = -2;
+    final int INVALID = Integer.MAX_VALUE;
     int target;
     int m;
     int n;
     int[] houses;
     int[][] cost;
-    Map<String, Integer> memo;
+    int [][][] memo;
     
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        memo = new HashMap<>();
+        memo = new int [m][n][101];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(memo[i][j], UNVISITED);
+            }
+        }
+        
         this.houses = houses;
         this.cost = cost;
         this.m = m;
         this.n = n;
         this.target = target;
-        return dfs (0, 0, 0);
+        int res = dfs (0, 0, 0);
+        return res == INVALID ? -1 : res;
     }
     
-    public int dfs (int i, int hoods, int prev) {
-        String key = "" + i + "," + prev + "," + hoods;
+    public int dfs (int i, int hoods, int prev) {       
+        if (i == m) {
+            return (hoods == target ? 0 : INVALID);
+        } else if (prev > 0 && memo[i][prev - 1][hoods] != UNVISITED) {
+            return memo[i][prev - 1][hoods];
+        } 
         
-        if (memo.containsKey(key)) {
-            return memo.get(key);
-        } else if (i == m) {
-            if (hoods == target) {
-                memo.put(key, 0);
-                return 0;
-            } else {
-                // didn't hit target number of neighborhoods
-                memo.put(key, -1);
-                return -1;
-            }
-        }
-        
-        int min = Integer.MAX_VALUE;
+        int min = INVALID;
         if (houses[i] == 0) {
             for (int j = 0; j < n; j++) {
                 int tail = dfs(i + 1, hoods + (prev == j + 1 ? 0 : 1), j + 1);
-                if (tail == -1) {
-                    // Didn't find the target number of neighborhoods
+                if (tail == INVALID)
                     continue;
-                }
                 min = Math.min(min, cost[i][j] + tail);
             }
         } else {
@@ -47,10 +45,8 @@ class Solution {
             min = dfs(i + 1, hoods + (prev == houses[i] ? 0 : 1), houses[i]);
         }
         
-        if (min == Integer.MAX_VALUE)
-            min = -1;
-        
-        memo.put(key, min);
+        if (prev > 0)
+            memo[i][prev - 1][hoods] = min;
         return min;
     }
 }
