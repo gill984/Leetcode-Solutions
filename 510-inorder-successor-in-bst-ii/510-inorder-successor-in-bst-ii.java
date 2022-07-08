5e2fc0a1ab76383de7b3b3756f754ed00a17f8d8
gill984 <mjgill984@gmail.com>
2022-07-08 00:58:01 -0400

    Time: 44 ms (49.45%), Space: 49.8 MB (19.73%) - LeetHub

--- 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.java
+++ 510-inorder-successor-in-bst-ii/510-inorder-successor-in-bst-ii.java
@@ -1,32 +1,20 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node parent;
-};
-*/
-
 class Solution {
     public Node inorderSuccessor(Node node) {
         // 3 cases
         // Either the node has a right node or it doesn't
         // If it has a right child we want the the leftmost node in the right subtree
         // Else keep going up the parent path until the node you come from is a left child, return that parent
-        if (node.right == null) {
+        if (node.right == null)
             return parentTrail(node.parent, node);
-        } else {
+        else
             return leftMost(node.right);
-        }
     }
     
     public Node parentTrail(Node parent, Node child) {
-        if (parent == null || parent.left == child) {
+        if (parent == null || parent.left == child)
             return parent;
-        } else {
+        else
             return parentTrail(parent.parent, parent);
-        }
     }
     
     public Node leftMost(Node n) {
