4f94ed23f5b9cc78df32442e6a22bf9869cace94
gill984 <mjgill984@gmail.com>
2022-07-08 01:26:24 -0400

    Time: 40 ms (64.06%), Space: 52.4 MB (35.00%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,45 +1,33 @@
 class Solution {
     final int UNVISITED = -1;
     final int INVALID = Integer.MAX_VALUE;
-    int target;
-    int m;
-    int n;
-    int[] houses;
-    int[][] cost;
-    int [][][] memo;
     
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        memo = new int [m][n][101];
+        int [][][] memo = new int [m][n][101];
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
                 Arrays.fill(memo[i][j], UNVISITED);
-        
-        this.houses = houses;
-        this.cost = cost;
-        this.m = m;
-        this.n = n;
-        this.target = target;
-        int res = dfs (0, 0, 0);
+        int res = dfs (0, 0, 0, houses, cost, target, memo, m, n);
         return res == INVALID ? -1 : res;
     }
     
-    public int dfs (int i, int hoods, int prev) {       
+    public int dfs (int i, int hoods, int prev, int [] h, int [][] cost, int target, int [][][] memo, int m, int n) {       
         if (i == m)
             return (hoods == target ? 0 : INVALID);
         else if (prev > 0 && memo[i][prev - 1][hoods] != UNVISITED)
             return memo[i][prev - 1][hoods];
         
         int min = INVALID;
-        if (houses[i] == 0) {
+        if (h[i] == 0) {
             for (int j = 0; j < n; j++) {
-                int tail = dfs(i + 1, hoods + (prev == j + 1 ? 0 : 1), j + 1);
+                int tail = dfs(i + 1, hoods + (prev == j + 1 ? 0 : 1), j + 1, h, cost, target, memo, m, n);
                 if (tail == INVALID)
                     continue;
                 min = Math.min(min, cost[i][j] + tail);
             }
         } else {
             // This house is already painted, no cost added from this house
-            min = dfs(i + 1, hoods + (prev == houses[i] ? 0 : 1), houses[i]);
+            min = dfs(i + 1, hoods + (prev == h[i] ? 0 : 1), h[i], h, cost, target, memo, m, n);
         }
         
         if (prev > 0)
