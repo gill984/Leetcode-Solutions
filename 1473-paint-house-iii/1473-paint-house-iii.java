9331e469774b6250c590bc97efc63a01e0a46272
gill984 <mjgill984@gmail.com>
2022-07-08 17:29:57 -0400

    Time: 48 ms (53.00%), Space: 47.8 MB (79.49%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -3,7 +3,7 @@ class Solution {
     final int INVALID = Integer.MAX_VALUE;
     
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        int [][][] memo = new int [m][n][101];
+        int [][][] memo = new int [m][n][target + 1];
         int res = dfs (0, 0, 0, houses, cost, target, memo, m, n);
         return res == INVALID ? -1 : res - 1;
     }
@@ -11,6 +11,8 @@ class Solution {
     public int dfs (int i, int hoods, int prev, int [] h, int [][] c, int t, int [][][] memo, int m, int n) {       
         if (i == m)
             return (hoods == t ? 1 : INVALID);
+        else if (hoods >= t + 1)
+            return INVALID;
         else if (prev > 0 && memo[i][prev - 1][hoods] != UNVISITED)
             return memo[i][prev - 1][hoods];
         
