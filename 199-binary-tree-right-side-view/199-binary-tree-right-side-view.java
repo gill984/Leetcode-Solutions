39f4a036bc47cc90c49410063f481caf857a1ae1
gill984 <mjgill984@gmail.com>
2022-07-10 21:17:34 -0400

    Time: 2 ms (46.58%), Space: 42.8 MB (41.09%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<int[]> depthToNode = new ArrayList<>();
-        dfs(root, depthToNode, 0, 1);
+        traverse(root, depthToNode, 0, 1);
         List<Integer> res = new ArrayList<>();
         for (int[] val : depthToNode)
             res.add(val[1]);
         return res;
     }
     
-    public void dfs (TreeNode n, List<int[]> depthToNode, int depth, int x) {
+    public void traverse (TreeNode n, List<int[]> depthToNode, int depth, int x) {
         if (n == null)
             return;
         
@@ -18,7 +18,7 @@ class Solution {
         else if (depthToNode.get(depth)[0] < x)
             depthToNode.set(depth, pair);
         
-        dfs (n.left, depthToNode, depth + 1, x * 2);
-        dfs (n.right, depthToNode, depth + 1, x * 2 + 1);
+        traverse(n.left, depthToNode, depth + 1, x * 2);
+        traverse(n.right, depthToNode, depth + 1, (x * 2) + 1);
     }
 }
