3f744e6c8f5b5836be07f5c2b9e22f3dc5678afc
gill984 <mjgill984@gmail.com>
2022-07-11 21:43:30 -0400

    Time: 145 ms (58.42%), Space: 39.6 MB (99.61%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,41 +1,47 @@
-public class Solution {
-    public boolean makesquare(int[] nums) {
-    	if (nums == null || nums.length < 4) return false;
-        int sum = 0;
-        for (int num : nums) sum += num;
-        if (sum % 4 != 0) return false;
+class Solution {
+    public boolean makesquare(int[] matchsticks) {
+        int length = 0;
+        for (int i : matchsticks)
+            length += i;
+        if (length % 4 != 0)
+            return false;
         
-        Arrays.sort(nums);
-        reverse(nums);
-        
-    	return dfs(nums, new int[4], 0, sum / 4);
+        length /= 4;    // length of each side
+        Arrays.sort(matchsticks);
+        reverse(matchsticks);
+        int n = matchsticks.length;
+        return search(matchsticks, 0, new int [4], length, n);
     }
     
-    private boolean dfs(int[] nums, int[] sums, int index, int target) {
-    	if (index == nums.length) {
-    	    if (sums[0] == target && sums[1] == target && sums[2] == target) {
-    		return true;
-    	    }
-    	    return false;
-    	}
-    	
-    	for (int i = 0; i < 4; i++) {
-    	    if (sums[i] + nums[index] > target) continue;
-    	    sums[i] += nums[index];
-            if (dfs(nums, sums, index + 1, target)) return true;
-    	    sums[i] -= nums[index];
-    	}
-    	
-    	return false;
+    boolean search (int [] matchsticks, int i, int[] sides, int length, int n) {
+        if (i == n) {
+            if (sides[0] == length && sides[1] == length && sides[2] == length)
+                return true;
+            return false;
+        }
+        
+        // Try putting this match in each spot
+        for (int s = 0; s < 4; s++) {
+            if (sides[s] + matchsticks[i] > length)
+                continue;
+            sides[s] += matchsticks[i];
+            if (search(matchsticks, i + 1, sides, length, n))
+                return true;
+            sides[s] -= matchsticks[i];
+        }
+        
+        return false;
     }
     
-    private void reverse(int[] nums) {
-        int i = 0, j = nums.length - 1;
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++; j--;
+    void reverse (int[] a) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo < hi) {
+            int temp = a[lo];
+            a[lo] = a[hi];
+            a[hi] = temp;
+            hi--;
+            lo++;
         }
     }
 }
