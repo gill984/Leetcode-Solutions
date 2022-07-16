07fd57c6f6663d116a36d90f441507b11403dd6e
gill984 <mjgill984@gmail.com>
2022-07-15 22:34:12 -0400

    Time: 7 ms (75.21%), Space: 42.3 MB (74.79%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.java
@@ -3,22 +3,24 @@ class Solution {
     int [][] dirs = new int [][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
         int res = 0;
-        int [][][] dp = new int [maxMove + 2][m][n];
-        dp[0][startRow][startColumn] = 1;
+        int [][] next = new int [m][n];
+        next[startRow][startColumn] = 1;
         
         for (int move = 0; move < maxMove; move++) {
+            int [][] curr = next;
+            next = new int [m][n];
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n; j++) {
-                    if (dp[move][i][j] == 0)
+                    if (curr[i][j] == 0)
                         continue;
                     
                     for (int [] dir : dirs) {
                         int nextRow = i + dir[0];
                         int nextCol = j + dir[1];
                         if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n)
-                            res = (res + dp[move][i][j]) % MOD;
+                            res = (res + curr[i][j]) % MOD;
                         else
-                            dp[move + 1][nextRow][nextCol] = (dp[move + 1][nextRow][nextCol] + dp[move][i][j]) % MOD;
+                            next[nextRow][nextCol] = (next[nextRow][nextCol] + curr[i][j]) % MOD;
                     }
                 }
                 
