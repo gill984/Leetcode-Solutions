e9ca73f57fbd508cdaba8cab22129c915d4d0d39
gill984 <mjgill984@gmail.com>
2022-07-19 12:40:02 -0400

    Time: 49 ms (46.28%), Space: 14 MB (17.88%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -4,9 +4,9 @@ class Solution:
             return []
         res = [[1]]
         for i in range(1, numRows):
-            row = [res[i-1][0]]
+            row = [0 for _ in range(i + 1)]
+            row[0], row[-1] = 1, 1
             for j in range(1, i):
-                row.append(res[i-1][j] + res[i-1][j-1])
-            row.append(res[i-1][-1])
+                row[j] = (res[i-1][j] + res[i-1][j-1])
             res.append(row)
         return res
