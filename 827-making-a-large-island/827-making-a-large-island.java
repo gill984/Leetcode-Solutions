a8f49ef83a80947a66a6aa848532a910ed02b034
gill984 <mjgill984@gmail.com>
2022-07-22 19:45:38 -0400

    Time: 149 ms (81.33%), Space: 71 MB (99.06%) - LeetHub

--- 827-making-a-large-island/827-making-a-large-island.java
+++ 827-making-a-large-island/827-making-a-large-island.java
@@ -18,7 +18,6 @@ class Solution {
                 }
             }
         }
-        System.out.println(islandSize);
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
