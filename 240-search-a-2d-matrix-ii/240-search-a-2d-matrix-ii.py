c66f119978e4b73d5656a5f778d45e6c4cbaf769
gill984 <mjgill984@gmail.com>
2022-07-23 20:24:30 -0400

    Time: 197 ms (79.20%), Space: 20.5 MB (13.29%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for row in matrix:
+            if row[0] <= target and row[-1] >= target:
+                idx = bisect_left(row, target)
+                if row[idx] == target:
+                    return True
+        return False
+            
