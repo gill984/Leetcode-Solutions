236662b774bc527af0effdcadb7b4a289352f91e
gill984 <mjgill984@gmail.com>
2022-07-28 21:38:59 -0400

    Time: 14 ms (62.29%), Space: 51.1 MB (58.68%) - LeetHub

---
+++ 251-flatten-2d-vector/251-flatten-2d-vector.java
@@ -0,0 +1,36 @@
+class Vector2D {
+    int [][] vec;
+    int row;
+    int col;
+    
+    public Vector2D(int[][] vec) {
+        row = 0;
+        col = 0;
+        while (row < vec.length && col == vec[row].length) {
+            row += 1;
+            col = 0;
+        }
+        this.vec = vec;
+    }
+    
+    public int next() {
+        int res = vec[row][col];
+        col++;
+        while (row < vec.length && col == vec[row].length) {
+            row += 1;
+            col = 0;
+        }
+        return res;
+    }
+    
+    public boolean hasNext() {
+        return row != vec.length;
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D obj = new Vector2D(vec);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
