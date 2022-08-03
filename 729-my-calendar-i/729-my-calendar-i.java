0561d70d92462a9eaff136fa23da02c5e0e7404f
gill984 <mjgill984@gmail.com>
2022-08-02 21:29:22 -0400

    Time: 30 ms (75.04%), Space: 54.7 MB (35.87%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -8,9 +8,7 @@ class MyCalendar {
     public boolean book(int start, int end) {
         Map.Entry<Integer, Integer> floor = schedule.floorEntry(start);
         Map.Entry<Integer, Integer> ceil = schedule.ceilingEntry(start);
-        if (ceil != null && end > ceil.getKey())
-            return false;
-        else if (floor != null && floor.getValue() > start)
+        if (ceil != null && end > ceil.getKey() || floor != null && floor.getValue() > start)
             return false;
         schedule.put(start, end);
         return true;
