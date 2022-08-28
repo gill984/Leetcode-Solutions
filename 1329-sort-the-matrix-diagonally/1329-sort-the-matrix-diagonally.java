287d319b5af7ee6f4ba225988d6cd7d7f7a2c7ec
gill984 <mjgill984@gmail.com>
2022-08-27 21:22:41 -0400

    Time: 12 ms (47.83%), Space: 48.5 MB (27.78%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -0,0 +1,37 @@
+class Solution {
+    int m;
+    int n;
+    public int[][] diagonalSort(int[][] mat) {
+        this.m = mat.length;
+        this.n = mat[0].length;
+        
+        for (int i = 0; i < m; i++)
+            sortDiag(mat, i, 0);
+        
+        for (int i = 1; i < n; i++)
+            sortDiag(mat, 0, i);
+        
+        return mat;
+    }
+    
+    public void sortDiag(int[][] mat, int row, int col) {
+        List<Integer> diag = new ArrayList<>();
+        int tempRow = row;
+        int tempCol = col;
+        while (row < m && col < n) {
+            diag.add(mat[row][col]);
+            row++;
+            col++;
+        }
+        
+        Collections.sort(diag);
+        
+        row = tempRow;
+        col = tempCol;
+        for (int i : diag) {
+            mat[row][col] = i;
+            row++;
+            col++;
+        }
+    }
+}
