9439877dfd2bbaa85cd87ae76fbc8c1aff0594e2
gill984 <mjgill984@gmail.com>
2022-09-25 20:18:10 -0400

    Time: 3 ms (33.64%), Space: 42.8 MB (36.52%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean equationsPossible(String[] equations) {
+        // Union all equal objects
+        // Perform find on all non-equal and verify they aren't part of the same equation
+        List<String> notEquals = new ArrayList<>();
+        DisjointSet ds = new DisjointSet();
+        for (String equation : equations) {
+            if (equation.contains("!")) {
+                notEquals.add(equation);
+            } else {
+                ds.union(equation.charAt(0), equation.charAt(3));
+            }
+        }
+        
+        for (String equation : notEquals) {
+            if (ds.find(equation.charAt(0)) == ds.find(equation.charAt(3))) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
+class DisjointSet {
+    int [] p;
+    int size = 26;
+    
+    public DisjointSet () {
+        p = new int [size];
+        for (int i = 0; i < size; i++) {
+            p[i] = i;
+        }
+    }
+    
+    public int find(char c) {
+        int a = c - 'a';
+        while (a != p[a]) {
+            p[a] = p[p[a]];
+            a = p[a];
+        }
+        return a;
+    }
+    
+    public void union(char c1, char c2) {
+        int a = find(c1);
+        int b = find(c2);
+        p[a] = b;
+    }
+}
