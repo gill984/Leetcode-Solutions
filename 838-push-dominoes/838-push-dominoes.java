aff1c81ba0705261a4eadff1ce1c5ae728d4c1ef
gill984 <mjgill984@gmail.com>
2022-09-26 22:55:05 -0400

    Time: 20 ms (86.99%), Space: 54.9 MB (24.25%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -9,32 +9,30 @@ class Solution {
             char c = dominoes.charAt(hi);
             if (c == 'L') {
                 if (dominoes.charAt(lo) == '.' || dominoes.charAt(lo) == 'L') {
-                    for (int i = lo; i < hi; i++) {
-                        res[i] = 'L';
-                    }
+                    fill(lo, hi, res, 'L');
                 } else {
                     split(lo, hi, res);
                 }
                 lo = hi;
             } else if (c == 'R') {
-                if (dominoes.charAt(lo) == 'R') {
-                    for (int i = lo; i < hi; i++) {
-                        res[i] = 'R';
-                    }
-                }
+                if (dominoes.charAt(lo) == 'R')
+                    fill(lo, hi, res, 'R');
                 lo = hi;
             }
             
             hi++;
         }
         
-        if (res[lo] == 'R') {
-            for (int i = lo; i < hi; i++) {
-                res[i] = 'R';
-            }
-        }
+        if (res[lo] == 'R')
+            fill(lo, hi, res, 'R');
         
         return new String(res);
+    }
+    
+    public void fill(int lo, int hi, char [] res, char c) {
+        for (int i = lo; i < hi; i++) {
+            res[i] = c;
+        }
     }
     
     public void split(int lo, int hi, char [] res) {
