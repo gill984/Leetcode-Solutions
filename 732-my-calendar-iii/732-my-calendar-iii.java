4c51b0d4eeb1d9e65b5708eb5b2cfeac92026a10
gill984 <mjgill984@gmail.com>
2022-10-07 17:22:12 -0400

    Time: 1105 ms (5.20%), Space: 118.1 MB (5.45%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.java
@@ -0,0 +1,25 @@
+class MyCalendarThree {
+    TreeMap<Integer, Integer> times;
+    
+    public MyCalendarThree() {
+        times = new TreeMap<>();
+    }
+    
+    public int book(int start, int end) {
+        times.put(start, times.getOrDefault(start, 0) + 1);
+        times.put(end, times.getOrDefault(end, 0) - 1);
+        int res = 0;
+        int count = 0;
+        for (int t : times.keySet()) {
+            count += times.get(t);
+            res = Math.max(count, res);
+        }
+        return res;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ */
