a79be8648186e70fc99fc7391a012c3649a7f2d8
gill984 <mjgill984@gmail.com>
2022-11-12 22:08:14 -0500

    Time: 21 ms (37.89%), Space: 45.1 MB (33.63%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -3,8 +3,8 @@ class Solution {
         Deque<Character> stack = new ArrayDeque<>();
         int n = s.length();
         StringBuilder res = new StringBuilder();
-        for (int i = n - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ') {
+        for (int i = n - 1; i >= -1; i--) {
+            if (i >= 0 && s.charAt(i) != ' ') {
                 stack.addLast(s.charAt(i));
             } else if (!stack.isEmpty()) {
                 if (res.length() != 0) {
@@ -15,14 +15,6 @@ class Solution {
                     res.append(stack.removeLast());
                 }
             }
-        }
-        
-        if (!stack.isEmpty() && res.length() != 0) {
-            res.append(' ');
-        }
-
-        while (!stack.isEmpty()) {
-            res.append(stack.removeLast());
         }
         
         return res.toString();
