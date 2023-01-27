74c88bcb8b948c83a0d926b480c71d71ad8f8131
gill984 <mjgill984@gmail.com>
2023-01-26 22:41:49 -0500

    Time: 84 ms (46.11%), Space: 50 MB (57.71%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,69 +1,45 @@
 class Solution {
+    Map<String, Boolean> memo;
+    Set<String> wordSet;
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        // Bucket sort words by first char
-        List<List<String>> buckets = new ArrayList<>();
-        for (int i = 0; i < 26; i++)
-            buckets.add(new ArrayList<>());
-        for (String word : words)
-            buckets.get(word.charAt(0) - 'a').add(word);
-
-        // Build trie
-        TrieNode root = new TrieNode();
-        for (String word : words) {
-            TrieNode curr = root;
-
-            for (char c : word.toCharArray()) {
-                if (curr.children[c - 'a'] == null)
-                    curr.children[c - 'a'] = new TrieNode();
-                curr = curr.children[c - 'a'];
+        List<String> result = new ArrayList<String>();
+        if (words == null || words.length == 1)
+            return result;
+        
+        wordSet = new HashSet<String>();
+        memo = new HashMap<String, Boolean>();
+        
+        for(String word : words)
+            wordSet.add(word);
+        
+        for(String word : words)
+            if(recurseDivide(word) && !word.equals("") )
+                result.add(word);
+        
+        return result;
+    }
+    
+    public boolean recurseDivide(String s)
+    {
+        if(s == null || s.length() == 0)
+            return false;
+        
+        // If we already searched this string
+        if (memo.containsKey(s))
+            return memo.get(s);
+            
+        for(int l = 1; l < s.length(); l++)
+        {
+            String left = s.substring(0, l);
+            String right = s.substring(l, s.length());
+            if(wordSet.contains(left) && (wordSet.contains(right) || recurseDivide(right)))
+            {
+                memo.put(s, true);
+                return true;
             }
-
-            curr.endWord = word;
         }
-
-
-        // Traverse trie, try each word as the first traversal
-        List<String> res = new ArrayList<>();
-        for (String word : words)
-            traverse(word, 0, false, root.children[word.charAt(0) - 'a'], buckets, res);
-
-        return res;
-    }
-
-    public void traverse(String word, int idx, boolean isConcatenated, TrieNode curr, List<List<String>> buckets, List<String> res) {
-        if (curr == null)
-            return;
-
-        if (idx != word.length() - 1) {
-            traverse(word, idx + 1, isConcatenated, curr.children[word.charAt(idx + 1) - 'a'], buckets, res);
-            return;
-        }
-
-        if (curr.visited)
-            return;
-
-        if (isConcatenated)
-            curr.visited = true;
-
-        if(isConcatenated && curr.endWord != null)
-            res.add(curr.endWord);
-
-        // Traverse out from here using all possible next first characters
-        for (int i = 0; i < 26; i++) {
-            if (curr.children[i] == null)
-                continue;
-
-            for (String nextWord : buckets.get(i))
-                traverse(nextWord, 0, true, curr.children[i], buckets, res);
-        }
-    }
-
-    class TrieNode {
-        TrieNode [] children = new TrieNode[26];
-        boolean visited = false;
-        String endWord = null;
-
-        public TrieNode() {
-        }
+        
+        memo.put(s, false);
+        return false;
     }
 }
