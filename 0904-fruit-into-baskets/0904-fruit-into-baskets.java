117d79d260c9285f7657a09fc4684c4af300ea2c
gill984 <mjgill984@gmail.com>
2023-02-06 22:59:02 -0500

    Time: 6 ms (96.47%), Space: 51.8 MB (56.10%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,54 +1,25 @@
-
 class Solution {
     public int totalFruit(int[] fruits) {
-        int nextLo = 0;
+        int chainStart = 0;
+        int basket1 = -1;
+        int basket2 = -1;
         int n = fruits.length;
-        if (n == 1)
-            return 1;
-        int res = 2;
-        
-        while (nextLo < n)
-        {
-            int lo = nextLo;
-            int hi = lo + 1;
-            int first = fruits[lo];
-            int second = -1;
-            while (hi < n)
-            {
-                if (fruits[hi] == first)
-                {
-                    if (second != -1 && fruits[nextLo] != first)
-                        nextLo = hi;
-                    hi++;
-                }
-                else if (fruits[hi] == second)
-                {
-                    if (fruits[nextLo] != second)
-                        nextLo = hi;
-                    hi++;
-                }
-                else if (second != -1 && fruits[hi] != second)
-                {
-                    hi--;
-                    res = Math.max(hi - lo + 1, res);
-                    break;
-                }
-                else if (second == -1)
-                {
-                    second = fruits[hi];
-                    nextLo = hi;
-                    hi++;
-                }
+        int res = 0;
+        int lo = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (basket1 != fruits[i] && basket2 != fruits[i]) {
+                lo = chainStart;
+                if (basket1 != fruits[chainStart])
+                    basket1 = fruits[i];
+                else
+                    basket2 = fruits[i];
             }
-            
-            if (hi == n)
-            {
-                res = Math.max(hi - lo, res);
-                break;
-            }
+            if (fruits[chainStart] != fruits[i])
+                chainStart = i;
+            res = Math.max(res, i - lo + 1);
         }
-        
-        
+
         return res;
     }
 }
