e5669a3ff2527d83ef8e03de732bdd64482c79a3
gill984 <mjgill984@gmail.com>
2023-02-06 22:58:29 -0500

    Time: 6 ms (96.47%), Space: 51.6 MB (67.94%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int chainStart = 0;
+        int basket1 = -1;
+        int basket2 = -1;
+        int n = fruits.length;
+        int res = 0;
+        int lo = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (basket1 != fruits[i] && basket2 != fruits[i]) {
+                lo = chainStart;
+                if (basket1 != fruits[chainStart])
+                    basket1 = fruits[i];
+                else
+                    basket2 = fruits[i];
+            }
+            if (fruits[chainStart] != fruits[i])
+                chainStart = i;
+            res = Math.max(res, i - lo + 1);
+        }
+
+        return res;
+    }
+}
