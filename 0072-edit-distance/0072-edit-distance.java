62fd98018c2fffeefb9677e5c9ec5ded6de0f03d
gill984 <mjgill984@gmail.com>
2023-02-26 12:04:03 -0500

    Time: 6 ms (67.73%), Space: 42.9 MB (22.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -12,20 +12,13 @@ class Solution
             dp[0][j] = j;
 
         for (int i = 0; i < word1.length(); i++)
-        {
             for (int j = 0; j < word2.length(); j++)
-            {
                 // First case is where character i matches character j, no edit needed
                 if (word1.charAt(i) == word2.charAt(j))
-                {
                     dp[i + 1][j + 1] = dp[i][j];
-                } else
-                {
+                else
                     // Second case is where an edit is needed, either insert, delete, or modify
                     dp[i + 1][j + 1] = 1 + Math.min(dp[i][j], Math.min(dp[i][j + 1], dp[i + 1][j]));
-                }
-            }
-        }
 
         return dp[word1.length()][word2.length()];
     }
