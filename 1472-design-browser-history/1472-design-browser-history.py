31d99f8b81f99397f4d7cd0bd089449ec4613194
gill984 <mjgill984@gmail.com>
2023-03-18 01:09:45 -0400

    Time: 226 ms (74.42%), Space: 16.6 MB (85.99%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,31 +1,34 @@
 class BrowserHistory:
-
     def __init__(self, homepage: str):
         self.history = [homepage]
         self.current_idx = 0
+        self.max_forward_idx = 0
 
     def visit(self, url: str) -> None:
-        # Clear the forward history
-        self.history = self.history[:self.current_idx+1]
-        # Add the new url to the history
-        self.history.append(url)
-        # Update the current index
+        # If current_idx is already at the end of the history list, append the new url
+        if self.current_idx == len(self.history) - 1:
+            self.history.append(url)
+        # If current_idx is not at the end of the history list, overwrite the value and add 1 to current_idx
+        else:
+            self.history[self.current_idx+1] = url
+            self.history = self.history[:self.current_idx+2]
+        # Update the indices
         self.current_idx = len(self.history) - 1
+        self.max_forward_idx = self.current_idx
 
     def back(self, steps: int) -> str:
-        # Move back in history
-        self.current_idx = max(0, self.current_idx - steps)
+        # Calculate the new index after moving back
+        new_idx = max(0, self.current_idx - steps)
+        # Update the current and max forward indices
+        self.max_forward_idx = max(self.max_forward_idx, self.current_idx)
+        self.current_idx = new_idx
+        # Return the current url
         return self.history[self.current_idx]
 
     def forward(self, steps: int) -> str:
-        # Move forward in history
-        self.current_idx = min(len(self.history) - 1, self.current_idx + steps)
+        # Calculate the new index after moving forward
+        new_idx = min(self.max_forward_idx, self.current_idx + steps)
+        # Update the current index
+        self.current_idx = new_idx
+        # Return the current url
         return self.history[self.current_idx]
-        
-
-
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
