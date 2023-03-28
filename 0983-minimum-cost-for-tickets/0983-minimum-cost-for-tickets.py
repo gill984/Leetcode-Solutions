efc6db9e37e05f2210e47aaa94133d6c5d186b9c
gill984 <mjgill984@gmail.com>
2023-03-27 21:41:57 -0400

    Time: 63 ms (18.02%), Space: 13.9 MB (83.75%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp = [sys.maxsize for _ in range(0, days[-1] + 1)]
+        dp[0] = 0
+        for idx, day in enumerate(days):
+            prev_cost = dp[days[idx - 1]] if idx > 0 else 0
+            dp[day] = min(dp[day], prev_cost + costs[0])
+            for j in range(day, min(day + 7, len(dp))):
+                dp[j] = min(dp[j], prev_cost + costs[1])
+            for j in range(day, min(day + 30, len(dp))):
+                dp[j] = min(dp[j], prev_cost + costs[2])
+        
+        # print(dp)
+        return dp[-1];
+        
