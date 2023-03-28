95d60e1087170f4e783185911509ef9a9c7ea253
gill984 <mjgill984@gmail.com>
2023-03-27 21:27:42 -0400

    Time: 3 ms (21.01%), Space: 41.1 MB (34.50%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -6,30 +6,14 @@ class Solution {
         
         for (int i = 0; i < n; i++) {
             int day = days[i];
-            
             int prevCost = (i > 0 ? dp[days[i - 1]] : 0);
-            int singleCost = prevCost + costs[0];
-            int sevenCost = prevCost + costs[1];
-            int thirtyCost = prevCost + costs[2];
             
-            // System.out.println("day: " + day);
-            // System.out.println("prevcost: " + prevCost);
-            // System.out.println("singleCost: " + singleCost);
-            // System.out.println("sevenCost: " + sevenCost);
-            // System.out.println("thirtyCost: " + thirtyCost);
-            
-            dp[day] = Math.min(dp[day], singleCost);
-            
-            // Fill in dp with 7 and 30 day prices
-            for (int j = 0; j < 7 && j + day < dp.length; j++) {
-                dp[j + day] = Math.min(dp[j + day], sevenCost);
-            }
-            
-            for (int j = 0; j < 30 && j + day < dp.length; j++) {
-                dp[j + day] = Math.min(dp[j + day], thirtyCost);
-            }
-            
-            // System.out.println(Arrays.toString(dp));
+            // Fill in dp for single day, 7-day, and 30-day ticket options
+            dp[day] = Math.min(dp[day], prevCost + costs[0]);
+            for (int j = 0; j < 7 && j + day < dp.length; j++)
+                dp[j + day] = Math.min(dp[j + day], prevCost + costs[1]);
+            for (int j = 0; j < 30 && j + day < dp.length; j++)
+                dp[j + day] = Math.min(dp[j + day], prevCost + costs[2]);
         }
         
         return dp[dp.length - 1];
