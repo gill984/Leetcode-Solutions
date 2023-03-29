66890587384201515d774264d71e27a71151961c
gill984 <mjgill984@gmail.com>
2023-03-28 20:44:39 -0400

    Time: 3 ms (54.38%), Space: 40.6 MB (73.20%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -4,17 +4,10 @@ class Solution {
         int res = 0;
         int n = satisfaction.length;
         for (int i = 0; i < n; i++) {
-            res = Math.max(res, calculateSatisfaction(satisfaction, i, satisfaction.length - 1));
-        }
-        
-        return res;
-    }
-    
-    public int calculateSatisfaction(int [] s, int i, int j) {
-        int res = 0;
-        
-        for (int idx = i, time = 1; idx <= j; idx++, time++) {
-            res += (time * s[idx]);
+            int sum = 0;
+            for (int j = i, time = 1; j < n; j++, time++)
+                sum += (time * satisfaction[j]);
+            res = Math.max(res, sum);
         }
         
         return res;
