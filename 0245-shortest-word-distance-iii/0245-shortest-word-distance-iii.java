23b048bb6368e766f813d5ffd5e8f2b131923310
gill984 <mjgill984@gmail.com>
2023-03-31 20:19:45 -0400

    Time: 14 ms (60.42%), Space: 56.6 MB (62.08%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.java
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.java
@@ -7,35 +7,22 @@ class Solution {
         boolean sameWord = word1.equals(word2);
         for (int i = 0; i < wordsDict.length; i++) {
             String s = wordsDict[i];
-            if (s.equals(word1)) {
-                
-                if (!sameWord) {
+            
+            if (sameWord && s.equals(word1)) {
+                if (assignOne)
                     idx1 = i;
-                }
+                else
+                    idx2 = i;
                 
-                if (sameWord && assignOne) {
-                    idx1 = i;
-                    assignOne = false;
-                    if (idx1 != -1 && idx2 != -1) {
-                        res = Math.min(res, Math.abs(idx1 - idx2));
-                    }
-                    continue;
-                }
+                assignOne = !assignOne;
             }
             
-            if (s.equals(word2)) {
-                if (!sameWord) {
-                    idx2 = i;
-                }
-                
-                if (sameWord && !assignOne) {
-                    idx2 = i;
-                    assignOne = true;
-                }
-                if (idx1 != -1 && idx2 != -1) {
-                    res = Math.min(res, Math.abs(idx1 - idx2));
-                }
-                continue;
+            if (!sameWord && s.equals(word1)) {
+                idx1 = i;
+            }
+            
+            if (!sameWord && s.equals(word2)) {
+                idx2 = i;
             }
             
             if (idx1 != -1 && idx2 != -1) {
