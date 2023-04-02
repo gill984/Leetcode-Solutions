8348f57951741c1c25545fbd367e09a7b207ecc7
gill984 <mjgill984@gmail.com>
2023-04-01 20:39:50 -0400

    Time: 92 ms (17.36%), Space: 62.9 MB (30.99%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -6,25 +6,19 @@ class Solution {
         int m = p.length;
         
         int [][] spells = new int [n][2];
-        int [][] potions = new int [m][2];
         for (int i = 0; i < n; i++) {
             spells[i][VAL] = s[i];
             spells[i][IDX] = i;
         }
         
-        for (int i = 0; i < m; i++) {
-            potions[i][VAL] = p[i];
-            potions[i][IDX] = i;
-        }
-        
         Arrays.sort(spells, (a, b) -> a[VAL] - b[VAL]);
-        Arrays.sort(potions, (a, b) -> a[VAL] - b[VAL]);
+        Arrays.sort(p);
         
         int [] res = new int [n];
         int pdx = 0;
         for (int i = n - 1; i >= 0; i--) {
             long spell = spells[i][VAL];
-            while (pdx < m && potions[pdx][VAL] * spell < success) {
+            while (pdx < m && p[pdx] * spell < success) {
                 pdx++;
             }
             
