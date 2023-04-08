254f8a019034f77d2b87fe3dbcf71b14a499fcd8
gill984 <mjgill984@gmail.com>
2023-04-08 11:07:21 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (22.43%) - LeetHub

---
+++ 0760-find-anagram-mappings/0760-find-anagram-mappings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] anagramMappings(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        Map<Integer, Integer> valToIndex = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            valToIndex.put(nums2[i], i);
+        }
+        
+        int [] mapping = new int [n];
+        for (int i = 0; i < n; i++) {
+            mapping[i] = valToIndex.get(nums1[i]);
+        }
+        return mapping;
+    }
+}
