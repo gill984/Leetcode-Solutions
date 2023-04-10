20eb0e9b5122d6b0c27791f2ee5d3a8346754fc7
gill984 <mjgill984@gmail.com>
2023-04-09 20:30:33 -0400

    Time: 1 ms (98.35%), Space: 40.9 MB (23.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,19 +2,16 @@ class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
+            if (c == '(' || c == '[' || c == '{')
                 stack.addLast(c);
-            } else if (stack.isEmpty()) {
+            else if (stack.isEmpty())
                 return false;
-            } else if (c == ')' && stack.peekLast() == '(') {
+            else if ((c == ')' && stack.peekLast() == '(') ||
+                       (c == ']' && stack.peekLast() == '[') || 
+                       (c == '}' && stack.peekLast() == '{'))
                 stack.removeLast();
-            } else if (c == ']' && stack.peekLast() == '[') {
-                stack.removeLast();
-            } else if (c == '}' && stack.peekLast() == '{') {
-                stack.removeLast();
-            } else {
+            else
                 return false;
-            }
         }
         
         return stack.isEmpty();
