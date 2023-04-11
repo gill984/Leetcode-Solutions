4d8fb31332c547eeddf95a843553be8493fd41c7
gill984 <mjgill984@gmail.com>
2023-04-10 20:08:52 -0400

    Time: 40 ms (86.96%), Space: 43.5 MB (81.67%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeStars(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == '*') {
+                res.deleteCharAt(res.length() - 1);
+            } else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
