4e52364f6c408edf9edda1fdc1519612eec32f72
gill984 <mjgill984@gmail.com>
2023-04-14 20:56:57 -0400

    Time: 226 ms (51.85%), Space: 44.8 MB (75.31%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,18 +1,29 @@
 class Solution {
+    int dp[][];
+    private int f(List<List<Integer>> piles, int i, int coins) {
+        if (i == 0) {
+            return 0;
+        }
+        if (dp[i][coins] != -1) {
+            return dp[i][coins];
+        }
+        int currentSum = 0;
+        for (int currentCoins = 0; currentCoins <= Math.min(piles.get(i - 1).size(), coins); currentCoins++) {
+            if (currentCoins > 0) {
+                currentSum += piles.get(i - 1).get(currentCoins - 1);
+            }
+            dp[i][coins] = Math.max(dp[i][coins], f(piles, i - 1, coins - currentCoins) + currentSum);
+        }
+        return dp[i][coins];
+    }
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
         int n = piles.size();
-        int dp[][] = new int[n + 1][k + 1];
+        dp = new int[n + 1][k + 1];
         for (int i = 1; i <= n; i++) {
             for (int coins = 0; coins <= k; coins++) {
-                int currentSum = 0;
-                for (int currentCoins = 0; currentCoins <= Math.min((int)piles.get(i - 1).size(), coins); currentCoins++) {
-                    if (currentCoins > 0) {
-                        currentSum += piles.get(i - 1).get(currentCoins - 1);
-                    }
-                    dp[i][coins] = Math.max(dp[i][coins], dp[i - 1][coins - currentCoins] + currentSum);
-                }
+                dp[i][coins] = -1;
             }
         }
-        return dp[n][k];
+        return f(piles, n, k);
     }
 }
