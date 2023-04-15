df514190b77f96a000e988b49b8a39540db7a20a
gill984 <mjgill984@gmail.com>
2023-04-15 09:38:06 -0400

    Time: 52 ms (95.06%), Space: 46.8 MB (38.89%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -1,35 +1,34 @@
 class Solution {
     public int maxValueOfCoins(List<List<Integer>> piles, int k) {
-        // dp[i][j] = max if we take only from piles 0-i and we take j top coins from pile i
+        // Compute presums, basically summed values if taking repeatedly from the same pile
+        // Use presum array as if it were input to the knapsack problem, consider the number
+        // of coins taken as the weight and presum value as the value.
         int n = piles.size();
         int [][] presums = new int [n][k + 1];
         
-        // Base case for first pile
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= piles.get(i).size() && j <= k; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 1; j <= piles.get(i).size() && j <= k; j++)
                 presums[i][j] = presums[i][j - 1] + piles.get(i).get(j - 1);
-            }
-        }
         
         int [] dp = new int [k + 1];
         Arrays.fill(dp, -1);
         dp[0] = 0;
         
         for (int i = 0; i < n; i++) {
+            // Need this temp array to stay the same value in a given row
+            // we can't decide to take a different number of coins from a single
+            // pile within 1 solution
             int [] temp = dp.clone();
-            for (int j = 1; j <= piles.get(i).size() && j <= k; j++) {
+            for (int j = 1; j <= piles.get(i).size() && j <= k; j++)
                 knapsackStep(presums, dp, temp, i, j);
-            }
         }
         
         return dp[k];
     }
     
     public void knapsackStep(int [][] presums, int [] dp, int [] temp, int row, int weight) {
-        for (int i = dp.length - 1; i >= 0; i--) {
-            if (temp[i] >= 0 && i + weight < dp.length) {
+        for (int i = dp.length - 1; i >= 0; i--)
+            if (temp[i] >= 0 && i + weight < dp.length)
                 dp[i + weight] = Math.max(dp[i + weight], temp[i] + presums[row][weight]);
-            }
-        }
     }
 }
