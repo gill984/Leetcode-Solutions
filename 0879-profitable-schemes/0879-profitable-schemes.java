4df323ea2b341886b78fe566ec722cd71f6128ee
gill984 <mjgill984@gmail.com>
2023-04-20 21:04:31 -0400

    Time: 30 ms (71.92%), Space: 40.7 MB (87.67%) - LeetHub

---
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -0,0 +1,34 @@
+class Solution {
+    int MOD = 1000000007;
+    
+    public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        // dp[i][j] = we made i profit using j total people so far in this many ways
+        int [][] dp = new int [minProfit + 1][n + 1];
+        dp[0][0] = 1;
+        
+        for (int i = 0; i < group.length; i++) {
+            for (int row = minProfit; row >= 0; row--) {
+                for (int col = n; col >= 0; col--) {
+                    if (dp[row][col] == 0)
+                        continue;
+                    
+                    int nextRow = Math.min(row + profit[i], minProfit);
+                    int nextCol = group[i] + col;
+
+                    if (nextCol > n)
+                        continue;
+
+                    dp[nextRow][nextCol] = (dp[row][col] + dp[nextRow][nextCol]) % MOD;
+                }
+            }
+        }
+        
+        // System.out.println(Arrays.deepToString(dp));
+        
+        int sum = 0;
+        for (int i : dp[minProfit]) {
+            sum = (sum + i) % MOD;
+        }
+        return sum;
+    }
+}
