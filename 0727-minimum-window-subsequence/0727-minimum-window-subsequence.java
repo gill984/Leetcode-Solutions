016c1ba9758f3108b240cedad67622b8b114ffbb
gill984 <mjgill984@gmail.com>
2023-04-23 16:49:22 -0400

    Time: 98 ms (47.90%), Space: 57.2 MB (5.09%) - LeetHub

--- 0727-minimum-window-subsequence/0727-minimum-window-subsequence.java
+++ 0727-minimum-window-subsequence/0727-minimum-window-subsequence.java
@@ -1,18 +1,20 @@
 class Solution {
+    int INVALID = -1;
+    
     public String minWindow(String s1, String s2) {
         // memo[i][j] = how many chars more necessary in s1 if we're at
         // char i and we've already matched j characters in s2
         int[][] memo = new int [s1.length() + 1][s2.length() + 1];
-        
         int res = Integer.MAX_VALUE;
         String stringRes = "";
         for (int i = 0; i < s1.length(); i++) {
-            int step = dfs(s1, s2, memo, i, 0);
-            if (step == -1)
-                continue;
-            if (step < res) {
-                stringRes = s1.substring(i, i + step);
-                res = step;
+            int iStartResult = dfs(s1, s2, memo, i, 0);
+            if (iStartResult == -1)
+                break;
+            
+            if (iStartResult < res) {
+                stringRes = s1.substring(i, i + iStartResult);
+                res = iStartResult;
             }
         }
         
@@ -27,14 +29,13 @@ class Solution {
             return memo[idx1][idx2] = 0;
         
         if (idx1 >= s1.length())
-            return memo[idx1][idx2] = -1;
+            return memo[idx1][idx2] = INVALID;
         
         int res = dfs(s1, s2, memo, idx1 + 1, idx2 + (s1.charAt(idx1) == s2.charAt(idx2) ? 1 : 0));
         
-        if (res == -1) {
-            return memo[idx1][idx2] = -1;
-        } else {
-            return memo[idx1][idx2] = res + 1;
-        }
+        if (res == INVALID)
+            return memo[idx1][idx2] = INVALID;
+        
+        return memo[idx1][idx2] = res + 1;
     }
 }
