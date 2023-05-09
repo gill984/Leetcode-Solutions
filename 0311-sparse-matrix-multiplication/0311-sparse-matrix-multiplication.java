21f6f32bfc725d3973f97a235571bdeed991f038
gill984 <mjgill984@gmail.com>
2023-05-09 17:38:58 -0400

    Time: 4 ms (20.99%), Space: 43.3 MB (44.85%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
@@ -1,23 +1,19 @@
 class Solution {
     public int[][] multiply(int[][] mat1, int[][] mat2) {
-        int n = mat1.length;
+        int m = mat1.length;
         int k = mat1[0].length;
-        int m = mat2[0].length;
+        int n = mat2[0].length;
         
-        // Product matrix will have 'n x m' size.
-        int[][] ans = new int[n][m];
+        int [][] res = new int [m][n];
         
-        for (int rowIndex = 0; rowIndex < n; ++rowIndex) {
-            for (int elementIndex = 0; elementIndex < k; ++elementIndex) {
-                // If current element of mat1 is non-zero then iterate over all columns of mat2.
-                if (mat1[rowIndex][elementIndex] != 0)  {
-                    for (int colIndex = 0; colIndex < m; ++colIndex) {
-                        ans[rowIndex][colIndex] += mat1[rowIndex][elementIndex] * mat2[elementIndex][colIndex];
-                    }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int z = 0; z < k; z++) {
+                    res[i][j] += (mat1[i][z] * mat2[z][j]);
                 }
             }
         }
         
-        return ans;
+        return res;
     }
 }
