08cd2f35a8e5eedadf3512e52c89de31ca22e798
gill984 <mjgill984@gmail.com>
2023-05-09 20:13:26 -0400

    Time: 0 ms (100.00%), Space: 43.5 MB (30.73%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.java
@@ -6,15 +6,11 @@ class Solution {
         
         int [][] res = new int [m][n];
         
-        for (int i = 0; i < m; i++) {
-            for (int z = 0; z < k; z++) {
-                if (mat1[i][z] != 0) {
-                    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++)
+            for (int z = 0; z < k; z++)
+                if (mat1[i][z] != 0)
+                    for (int j = 0; j < n; j++)
                         res[i][j] += (mat1[i][z] * mat2[z][j]);
-                    }
-                }
-            }
-        }
         
         return res;
     }
