167fd67bf28ee8a03765c3475f5e92e6ed3207cf
gill984 <mjgill984@gmail.com>
2023-05-13 01:52:29 -0400

    Time: 15 ms (48.67%), Space: 41.9 MB (56.64%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -5,21 +5,18 @@ class Solution {
         int MOD = 1000000007;
         
         for (int end = 1; end <= high; end++) {
-            if (end >= zero) {
+            if (end >= zero)
                 dp[end] = (dp[end] + dp[end - zero]) % MOD;
-            }
             
-            if (end >= one) {
+            if (end >= one)
                 dp[end] = (dp[end] + dp[end - one]) % MOD;
-            }
             
             dp[end] %= MOD;
         }
         
         int res = 0;
-        for (int i = low; i <= high; i++) {
+        for (int i = low; i <= high; i++)
             res = (res + dp[i]) % MOD;
-        }
         
         return res;
     }
