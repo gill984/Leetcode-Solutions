9b80d008212b857bca3c034ffc97b2272e510612
gill984 <mjgill984@gmail.com>
2023-05-23 22:13:36 -0400

    Time: 97 ms (81.61%), Space: 57.7 MB (71.26%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.java
@@ -0,0 +1,39 @@
+class Solution {
+    public long maxScore(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length;
+        
+        // Gradually allow smaller and smaller minimums
+        PriorityQueue<Integer> multipliers = new PriorityQueue<>();
+        int [][] nums = new int [n][2];
+        for (int i = 0; i < n; i++) {
+            nums[i][0] = nums1[i];
+            nums[i][1] = nums2[i];
+        }
+        
+        // Descending nums2 order, indexes 
+        Arrays.sort(nums, (a, b) -> b[1] - a[1] == 0 ? b[0] - a[0] : b[1] - a[1]);
+        // System.out.println(Arrays.deepToString(nums));
+        
+        // Initialize algorithm with starter length of k
+        long sum = 0;
+        long min = Integer.MAX_VALUE;
+        long res = 0;
+        
+        for (int i = 0; i < n; i++) {
+            min = Math.min(min, nums[i][1]);
+            if (multipliers.size() < k) {
+                multipliers.offer(nums[i][0]);
+                sum += nums[i][0];
+            } else if (nums[i][0] > multipliers.peek()) {
+                sum -= multipliers.poll();
+                sum += nums[i][0];
+                multipliers.offer(nums[i][0]);
+            }
+            
+            if (i >= k - 1)
+                res = Math.max(res, sum * min);
+        }
+        
+        return res;
+    }
+}
