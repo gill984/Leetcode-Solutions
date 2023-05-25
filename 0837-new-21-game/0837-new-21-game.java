c336143a2e921d2ae710c3f6ba45585988be863a
gill984 <mjgill984@gmail.com>
2023-05-24 22:57:55 -0400

    Time: 5 ms (6.02%), Space: 43.1 MB (7.23%) - LeetHub

--- 0837-new-21-game/0837-new-21-game.java
+++ 0837-new-21-game/0837-new-21-game.java
@@ -7,17 +7,6 @@ class Solution {
         double [] dp = new double [length];
         dp[0] = 1.00;
         
-//         double [] dp2 = new double [n + 1];
-//         dp2[0] = 1.00;
-//         for (int i = 1; i <= n; i++) {
-//             for (int j = Math.max(i - maxPts, 0); j < i; j++) {
-//                 if (j < k)
-//                     dp2[i] += (dp2[j] / maxPts);
-//             }
-//         }
-        
-//         System.out.println(Arrays.toString(dp2));
-        
         for (int i = 1; i < length; i++) {
             if (i > 1)
                 dp[i] += dp[i - 1];
@@ -28,8 +17,6 @@ class Solution {
             if (i - maxPts - 1 >= 0 && dp[i] > 0.0)
                 dp[i] -= dp[i - maxPts - 1] / maxPts;
         }
-        
-        // System.out.println(Arrays.toString(dp));
         
         double res = 0.0;
         dp[0] = 0.0;
