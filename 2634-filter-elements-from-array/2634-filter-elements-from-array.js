a3123c996b8f7c5af90aea134ab9ea51089e40c1
gill984 <mjgill984@gmail.com>
2023-06-01 12:24:06 -0400

    Time: 61 ms (39.87%), Space: 42.3 MB (24.37%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,27 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    s = 0;
-    shifts = Array(arr.length).fill(0);
-    length = 0;
+    res = [];
     
     for (let i = 0; i < arr.length; i++) {
-        if (!fn(arr[i], i)) {
-            s++;
-            shifts[i] = -1;
-        } else {
-            length++;
-            shifts[i] = s;
-        }
-    }
-    
-    res = Array(length);
-    for (let i = 0; i < shifts.length; i++) {
-        if (shifts[i] == -1) {
-            continue;
-        }
-        
-        res[i - shifts[i]] = arr[i];
+        if (fn(arr[i], i))
+            res.push(arr[i]);
     }
     return res;
 };
