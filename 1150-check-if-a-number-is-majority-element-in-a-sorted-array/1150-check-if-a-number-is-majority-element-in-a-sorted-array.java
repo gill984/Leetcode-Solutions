2e70c5cc85da88270b1901af9a21afaa3fdbdac2
gill984 <mjgill984@gmail.com>
2023-06-08 10:25:31 -0400

    Time: 0 ms (100.00%), Space: 40.8 MB (85.51%) - LeetHub

--- 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.java
+++ 1150-check-if-a-number-is-majority-element-in-a-sorted-array/1150-check-if-a-number-is-majority-element-in-a-sorted-array.java
@@ -1,30 +1,26 @@
 class Solution {
     public boolean isMajorityElement(int[] nums, int target) {
         int n = nums.length;
-        int leftIdx = binarySearchLeft(nums, target);
+        int leftIdx = binarySearchLeft(nums, target, n);
         
         if (leftIdx < 0 || leftIdx >= n || nums[leftIdx] != target)
             return false;
         
         int rightIdx = leftIdx + n / 2;
-        if (rightIdx < n && nums[rightIdx] == target)
-            return true;
-        return false;
+        return rightIdx < n && nums[rightIdx] == target;
     }
     
-    public int binarySearchLeft(int [] nums, int target) {
-        int n = nums.length;
+    public int binarySearchLeft(int [] nums, int target, int n) {
         int lo = 0;
         int hi = n - 1;
         
         while (lo <= hi) {
             int mid = (hi + lo) / 2;
             int val = nums[mid];
-            if (val >= target) {
+            if (val >= target)
                 hi = mid - 1;
-            } else {
+            else
                 lo = mid + 1;
-            }
         }
         
         return lo;
